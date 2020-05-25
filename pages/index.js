838059dccd8b88e8dcb7c48f7b40436f848705bf
Greg Rickaby <greg@gregrickaby.com>
2020-05-25 08:24:24 -0500

    semantic html and sticky sidebar

--- pages/index.js
+++ pages/index.js
@@ -13,26 +13,29 @@ const Homepage = () => {
 
   if (!isLoading) {
     content = (
-      <div className="flex space-x-8">
-        <div className="text-lg w-1/4 min-h-screen relative">
-          <div className="sticky">
-            <p>Enter the name of a subreddit, e.g., apple</p>
+      <section className="flex space-x-8">
+        <aside className="text-lg w-1/4 min-h-screen relative">
+          <div className="sticky top-0">
+            <h1 className="text-3xl mb-4">Reddit Viewer</h1>
             <input
-              className="border-solid border-2 border-gray-600 p-2"
+              className="border-solid border-2 border-gray-600 p-2 mb-2"
               type="text"
               name="subreddit"
               value={values.subreddit}
               onChange={handleChange}
             />
+            <p className="text-sm">
+              <em>Enter the name of a subreddit</em>
+            </p>
           </div>
-        </div>
+        </aside>
 
-        <div className="display">
+        <main>
           {data.children.map((post, index) => (
             <Card key={index} data={post} />
           ))}
-        </div>
-      </div>
+        </main>
+      </section>
     )
   }
 
