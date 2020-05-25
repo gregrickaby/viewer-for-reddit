60986f67399ad0131d36fcec62c30898bd556cc1
Greg Rickaby <greg@gregrickaby.com>
2020-05-25 08:52:42 -0500

    lazy load and infinite scroll

--- components/Card.js
+++ components/Card.js
@@ -1,17 +1,25 @@
+import PropTypes from 'prop-types'
+import Img from 'react-cool-img'
+
 const Card = (props) => {
   const post = props.data.data
   return (
-    <article className="card">
-      {post.thumbnail.length > 0 && (
-        <img src={post.thumbnail} alt={post.title} />
-      )}
+    <article className="w-full  bg-gray-400">
       <h2>
         <a href={post.url}>{post.title}</a>
       </h2>
+      {post.thumbnail.length > 0 && (
+        <Img src={post.thumbnail} alt={post.title} />
+      )}
       <p dangerouslySetInnerHTML={{__html: post.selftext}} />
       <span>{post.ups}</span>
+      <span>{post.downs}</span>
     </article>
   )
+}
+
+Card.propTypes = {
+  data: PropTypes.object
 }
 
 export default Card
