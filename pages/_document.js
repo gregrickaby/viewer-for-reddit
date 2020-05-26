6a468066c12da4db30fba19cf66674f0a644fdda
Greg Rickaby <greg@gregrickaby.com>
2020-05-26 09:49:38 -0500

    remove padding

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
-        <body className="p-4">
+        <body>
           <Main />
           <NextScript />
         </body>
