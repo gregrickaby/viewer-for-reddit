9d68603b14527e58d68ba6364dbe1d46f04844e1
Greg Rickaby <greg@gregrickaby.com>
2020-05-25 20:15:38 -0500

    refactor to use useDebounce

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,28 @@
-import {useFetch, useForm} from '@/lib/hooks'
+import {useState, useEffect} from 'react'
+import {useDebounce} from '@/lib/hooks'
 import Card from '@/components/Card'
 
 const Homepage = () => {
-  const [values, handleChange] = useForm({
-    subreddit: 'astrophotography'
-  })
+  const [searchTerm, setSearchTerm] = useState('astrophotography')
+  const [results, setResults] = useState()
+  const [loading, setLoading] = useState(true)
+  const debouncedSearchTerm = useDebounce(searchTerm, 750)
 
-  const [{data}, loading] = useFetch(
-    `https://www.reddit.com/r/${values.subreddit}/.json?limit=200&show=all`
-  )
+  useEffect(() => {
+    async function fetchData() {
+      if (debouncedSearchTerm) {
+        const response = await fetch(
+          `https://www.reddit.com/r/${searchTerm}/.json?limit=200&show=all`
+        )
+        const data = await response.json()
+        setResults(data)
+        setLoading(false)
+      } else {
+        setResults('astrophotography')
+      }
+    }
+    fetchData()
+  }, [debouncedSearchTerm]) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <section className="flex space-x-8">
@@ -18,9 +32,8 @@ const Homepage = () => {
           <input
             className="border-solid border-2 border-gray-600 p-2 mb-2"
             type="text"
-            name="subreddit"
-            value={values.subreddit}
-            onChange={handleChange}
+            placeholder="astrophotography"
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
           <p className="text-sm">
             <em>Enter the name of a subreddit</em>
@@ -31,7 +44,9 @@ const Homepage = () => {
         {loading ? (
           <p>Loading sub...</p>
         ) : (
-          data.children.map((post, index) => <Card key={index} data={post} />)
+          results.data.children.map((post, index) => (
+            <Card key={index} data={post} />
+          ))
         )}
       </main>
     </section>
