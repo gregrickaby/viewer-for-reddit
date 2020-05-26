32bfe0a8841428b915425d77a31b91ff57394918
Greg Rickaby <greg@gregrickaby.com>
2020-05-26 17:14:48 -0500

    begin plucking out and displaying different media types

--- components/Card.js
+++ components/Card.js
@@ -4,6 +4,7 @@ import Img from 'react-cool-img'
 const Card = (props) => {
   const post = props.data.data
   const date = new Date(post.created_utc * 1000)
+  const permalink = `https://www.reddit.com${post.permalink}`
 
   return (
     <article className="card">
@@ -18,20 +19,48 @@ const Card = (props) => {
       </header>
 
       <div className="card-content">
-        {post.thumbnail.length > 0 && (
-          <a href={post.url}>
-            <Img className="card-image" src={post.url} alt={post.title} />
-          </a>
-        )}
+        {(() => {
+          // Determin the media type using post_hint
+          switch (post.post_hint) {
+            case 'image':
+              return (
+                <Img className="card-image" src={post.url} alt={post.title} />
+              )
+            case 'hosted:video':
+              return (
+                <video
+                  src={post.secure_media.reddit_video.fallback_url}
+                  autoPlay="true"
+                ></video>
+              )
+            case 'rich:video':
+              return (
+                <div
+                  dangerouslySetInnerHTML={{__html: post.media_embed.content}}
+                />
+              )
+            case 'self':
+              return ''
+            case 'link':
+              return ''
+            default:
+              break
+          }
+        })()}
       </div>
 
       <footer className="card-footer">
         <div className="card-date">
-          Posted on {date.getMonth()}/{date.getDate()}/
-          {date.getFullYear().toString().slice(-2)}
+          By <code>{post.author}</code> on{' '}
+          <time>
+            {date.getMonth()}/{date.getDate()}/
+            {date.getFullYear().toString().slice(-2)}
+          </time>
         </div>
         <div className="card-votes">&uarr; {post.ups}</div>
-        <div className="card-comments">{post.num_comments} comments</div>
+        <div className="card-comments">
+          <a href={permalink}>{post.num_comments} comments</a>
+        </div>
       </footer>
     </article>
   )
