bfcf68ea738ae5be3f76bb165e0827c0b57541d3
Greg Rickaby <greg@gregrickaby.com>
2020-05-28 12:53:57 -0500

    display CORS message 5 seconds

--- components/Spinner.js
+++ components/Spinner.js
@@ -1,8 +1,23 @@
-const Spinner = () => (
-  <div className="spinner">
-    <p>Loading sub...</p>
-    <div className="sp sp-clock"></div>
-  </div>
-)
+import {useState, useEffect} from 'react'
+import CorsMessage from './CorsMessage'
+
+const Spinner = () => {
+  const [seconds, setSeconds] = useState(1)
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSeconds(seconds + 1)
+    }, 1000)
+    return () => clearInterval(timer)
+  })
+
+  return (
+    <div className="spinner">
+      <p>Loading sub...</p>
+      <div className="sp sp-clock"></div>
+      {seconds > 5 && <CorsMessage />}
+    </div>
+  )
+}
 
 export default Spinner
