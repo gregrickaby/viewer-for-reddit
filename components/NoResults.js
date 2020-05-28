0b1741d2fd4bb3d8a95996d1e43c26d85af00773
Greg Rickaby <greg@gregrickaby.com>
2020-05-28 12:53:42 -0500

    remove CORS message from NoResults (they're not related)

--- components/NoResults.js
+++ components/NoResults.js
@@ -7,16 +7,6 @@ const NoResults = () => (
       className="no-results-image"
       src="https://media.giphy.com/media/h3v63bGeVb1pdgFtTx/giphy.gif"
     />
-    <p className="italic text-sm">
-      Note: If you&apos;re unable to see anything, it&apos;s probably because of{' '}
-      <a
-        className="underline"
-        href="https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS"
-      >
-        CORS
-      </a>
-      . Try turning off tracking protection and/or any ad blocker extensions.
-    </p>
   </div>
 )
 
