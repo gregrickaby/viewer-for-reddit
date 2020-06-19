d149f89bae4df7b6fbc3d33d3581282226d871f4
Greg Rickaby <greg@gregrickaby.com>
2020-06-18 20:31:40 -0500

    setup SWR

--- pages/swr.js
+++ pages/swr.js
@@ -1,6 +1,6 @@
-import {Suspense, useState} from 'react'
+import {useState, useEffect} from 'react'
+import {useDebounce} from '@/lib/hooks'
 import useSWR from 'swr'
-
 import Card from '@/components/Card'
 import NoResults from '@/components/NoResults'
 import SiteHead from '@/components/SiteHead'
@@ -10,15 +10,22 @@ const fetcher = (url) => fetch(url).then((r) => r.json()) // eslint-disable-line
 
 const Posts = () => {
   const [searchTerm, setSearchTerm] = useState('itookapicture')
+  const [results, setResults] = useState()
+  const [loading, setLoading] = useState(true)
+  const debouncedSearchTerm = useDebounce(searchTerm, 1000)
+  const apiUrl = `https://cors-anywhere.herokuapp.com/https://www.reddit.com/r/${searchTerm}/.json?limit=200&show=all`
+  const {data, error} = useSWR(loading ? apiUrl : apiUrl, fetcher)
 
-  const {
-    data,
-    error
-  } = useSWR(
-    `https://www.reddit.com/r/${searchTerm}/.json?limit=200&show=all`,
-    fetcher,
-    {suspense: true}
-  )
+  useEffect(() => {
+    async function fetchData() {
+      if (debouncedSearchTerm) {
+        setResults(data)
+        setLoading(false)
+      }
+    }
+    fetchData()
+  }, [debouncedSearchTerm]) // eslint-disable-line react-hooks/exhaustive-deps
+
   if (error) return <NoResults />
 
   return (
@@ -40,18 +47,18 @@ const Posts = () => {
       </header>
 
       <main className="main wrap">
-        {data.data.children.map((post, index) => (
-          <Card key={index} data={post} />
-        ))}
+        {loading ? (
+          <Spinner />
+        ) : typeof results.data == 'undefined' ? (
+          <NoResults />
+        ) : (
+          results.data.children.map((post, index) => (
+            <Card key={index} data={post} />
+          ))
+        )}
       </main>
     </>
   )
 }
 
-export default function App() {
-  return (
-    <Suspense fallback={<Spinner />}>
-      <Posts />
-    </Suspense>
-  )
-}
+export default Posts
