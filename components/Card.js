0d58045563fcffd2389ce66b8c1b5279bcc21732
Greg Rickaby <greg@gregrickaby.com>
2020-06-19 20:34:28 -0500

    create a cleanIframe function

--- components/Card.js
+++ components/Card.js
@@ -1,26 +1,10 @@
 import PropTypes from 'prop-types'
 import Img from 'react-cool-img'
+import {cleanIframe} from '@/lib/functions'
 
 const Card = (props) => {
   const post = props.data.data
   const date = new Date(post.created_utc * 1000)
-
-  // Reddit encodes HTML, so decode it before using dangerouslySetInnerHTML.
-  // Plus, do a bunch of other clean ups.
-  // https://gomakethings.com/decoding-html-entities-with-vanilla-javascript/
-  const decodeHTML = (html) => {
-    const text = document.createElement('textarea') // eslint-disable-line no-undef
-    text.innerHTML = html
-    const sanitize = text.value.replace(
-      /(class="([^"]+)")|(width="([^"]+)")|(height="([^"]+)")|(title="([^"]+)")|(scrolling="([^"]+)")|(allow="([^"]+)")|(allowfullscreen="([^"]+)")|(allowfullscreen)|(style="([^"]+)")/gi,
-      ''
-    )
-    const iframe = sanitize.replace(
-      /(><\/iframe>)/gi,
-      'width="512" height="442" loading=lazy></iframe>'
-    )
-    return iframe
-  }
 
   return (
     <article className="card">
@@ -72,7 +56,7 @@ const Card = (props) => {
                   src={post.url}
                   className="card-embed"
                   dangerouslySetInnerHTML={{
-                    __html: decodeHTML(post.media.oembed.html)
+                    __html: cleanIframe(post.media.oembed.html)
                   }}
                 />
               )
