84af3cac53c9c71748637dce7c69278a34f0a9a5
Greg Rickaby <greg@gregrickaby.com>
2020-10-19 11:49:18 -0500

    add GA

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,22 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-1212017-88"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'UA-1212017-88');
+             `
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
