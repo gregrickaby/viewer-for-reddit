ee014ee7dbf68aafe89e95236247f0de0dbf7f68
Greg Rickaby <greg@gregrickaby.com>
2020-11-05 11:57:25 -0600

    update message

--- components/CorsMessage.js
+++ components/CorsMessage.js
@@ -3,14 +3,14 @@ export default function CorsMessage() {
     <div className="mt-4 italic text-sm max-w-md m-auto">
       <p className="text-lg font-bold mb-2">Not seeing anything?</p>
       <p>
-        Due to{' '}
+        Either Reddit&apos;s API is slow right now, or you may be experiencing{' '}
         <a
           className="underline"
           href="https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS"
         >
           CORS
         </a>{' '}
-        issues, you may need to disable tracking protection and your ad blocker
+        issues. You may need to disable tracking protection and your ad blocker
         for this website.
       </p>
     </div>
