d38a4acdc9e57c38443a95adfcae78f8a50b5e52
Greg Rickaby <greg@gregrickaby.com>
2020-11-20 14:58:41 -0600

    configure next/image

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  images: {
+    domains: [
+      'i.redd.it',
+      'i.reddit.com',
+      'redditmedia.com',
+      'reddit.com',
+      'a.thumbs.redditmedia.com',
+      'b.thumbs.redditmedia.com',
+      'i.imgur.com',
+      'imgur.com',
+      'media.giphy.com'
+    ]
+  }
+}
