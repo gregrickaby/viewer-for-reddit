3bb081675d5b8a7c78f430c5dba013b7a0173eaf
Greg Rickaby <gregrickaby@gmail.com>
2021-02-07 11:20:21 -0600

    add more domains

--- next.config.js
+++ next.config.js
@@ -9,7 +9,15 @@ module.exports = {
       'b.thumbs.redditmedia.com',
       'i.imgur.com',
       'imgur.com',
-      'media.giphy.com'
+      'media.giphy.com',
+      'media0.giphy.com',
+      'media1.giphy.com',
+      'media2.giphy.com',
+      'media3.giphy.com',
+      'media4.giphy.com',
+      'media5.giphy.com',
+      'live.staticflickr.com',
+      'cdn.embedly.com'
     ]
   }
 }
