be43ac3e838e078bc381eca6effaf250f22f1024
Greg Rickaby <gregrickaby@gmail.com>
2021-02-07 11:28:36 -0600

    bump colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 module.exports = {
-  purge: ['./components/**/*.js', './pages/**/*.js'],
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'media',
   theme: {
-    extend: {}
+    extend: {
+      container: {
+        center: true,
+        margin: 'auto'
+      },
+      fontFamily: {
+        comic: ['ComicNeue', 'sans-serif'],
+        dyslexic: ['OpenDyslexic', 'sans-serif']
+      },
+      colors: {
+        gray: {
+          200: '#d9d9d9',
+          500: '#2d2d2d',
+          900: '#111'
+        }
+      }
+    }
   },
   variants: {},
   plugins: [],
