c77b8c7d135bbf0f6e93b99f9871acbc5d1a8864
Greg Rickaby <gregrickaby@gmail.com>
2021-03-01 21:48:12 -0600

    create connector

---
+++ api/connector.js
@@ -0,0 +1,10 @@
+const snoowrap = require('snoowrap')
+
+const reddit = new snoowrap({
+  userAgent: 'reddit-image-viewer',
+  clientId: process.env.REDDIT_CLIENT_ID,
+  clientSecret: process.env.REDDIT_CLIENT_SECRET,
+  refreshToken: process.env.REDDIT_REFRESH_TOKEN
+})
+
+export default reddit
