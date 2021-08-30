ca1873c9e2240dcb81079842847be038b3c264f5
Greg Rickaby <gregrickaby@gmail.com>
2021-08-30 18:10:25 -0500

    create subreddit endpoint

--- pages/api/subreddit.js
+++ pages/api/subreddit.js
@@ -1,18 +1,24 @@
-import reddit from 'lib/reddit/connector'
+import reddit from 'lib/reddit'
 
 export default function subreddit(req, res) {
-  const subreddit = req.query.id
+  // Destructure the request.
+  const {name} = req.query
 
-  if (!subreddit) {
-    return res.status(400).send({
-      error:
-        'Please send a subreddit name in your request. /api/subreddit?id=SUBREDDIT_NAME'
-    })
+  try {
+    if (!name) {
+      return res.status(400).send({
+        error:
+          'Please send a subreddit name in your request. /api/subreddit?name=pics'
+      })
+    }
+
+    return reddit
+      .getSubreddit(name)
+      .getHot()
+      .then((response) => res.status(200).json(response))
+      .catch((error) => res.status(400).send(error))
+  } catch (err) {
+    // Issue? Leave a message and bail.
+    res.status(500).json({message: err.message})
   }
-
-  return reddit
-    .getSubreddit(subreddit)
-    .getHot()
-    .then((response) => res.status(200).json(response.map((post) => post.id)))
-    .catch((error) => res.status(400).send(error))
 }
