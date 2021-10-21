00f1f3994d864931cddbcd158d0fd3dcc303ec9c
Greg Rickaby <gregrickaby@gmail.com>
2021-10-21 16:50:10 -0500

    ignore onChange

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     '@next/next/no-img-element': 'off',
     'func-style': ['error', 'declaration'],
     'jsx-a11y/anchor-is-valid': 'off',
+    'jsx-a11y/no-onchange': 'off',
     'no-console': ['error', {allow: ['warn', 'error']}],
     'prettier/prettier': 'error'
   }
