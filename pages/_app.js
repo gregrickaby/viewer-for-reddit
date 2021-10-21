c57a8b8492a67f3b60bfdda37076a08ccc5fc74c
Greg Rickaby <gregrickaby@gmail.com>
2021-10-21 16:05:32 -0500

    enable jit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types'
 import 'styles/index.css'
-import 'styles/global.css'
 
 export default function App({Component, pageProps}) {
   return <Component {...pageProps} />
