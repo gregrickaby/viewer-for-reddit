cbf397deec45204ef6053ee7f9b35ac356432af1
Greg Rickaby <gregrickaby@gmail.com>
2021-12-26 13:04:18 -0600

    sr-only the loading header

--- components/Skeleton.js
+++ components/Skeleton.js
@@ -1,7 +1,7 @@
 export default function Skeleton() {
   return (
     <>
-      <h2 className="text-center text-2xl mb-4">Loading...</h2>
+      <h2 className="sr-only">Loading...</h2>
       <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 place-items-center">
         <div className="animate w-[28rem] h-[28rem]"></div>
         <div className="animate w-[28rem] h-[28rem]"></div>
