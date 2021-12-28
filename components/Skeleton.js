f7745fe56029237173531709d9fbcae165fb12c3
Greg Rickaby <gregrickaby@gmail.com>
2021-12-28 15:27:16 -0600

    refactor skeleton

--- components/Skeleton.js
+++ components/Skeleton.js
@@ -2,16 +2,11 @@ export default function Skeleton() {
   return (
     <>
       <h2 className="sr-only">Loading...</h2>
-      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 place-items-center">
-        <div className="animate w-[28rem] h-[28rem]"></div>
-        <div className="animate w-[28rem] h-[28rem]"></div>
-        <div className="animate w-[28rem] h-[28rem]"></div>
-        <div className="animate w-[28rem] h-[28rem]"></div>
-        <div className="animate w-[28rem] h-[28rem]"></div>
-        <div className="animate w-[28rem] h-[28rem]"></div>
-        <div className="animate w-[28rem] h-[28rem]"></div>
-        <div className="animate w-[28rem] h-[28rem]"></div>
-        <div className="animate w-[28rem] h-[28rem]"></div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 place-items-center gap-4">
+        <div className="skeleton-bone"></div>
+        <div className="skeleton-bone"></div>
+        <div className="skeleton-bone"></div>
+        <div className="skeleton-bone"></div>
       </div>
     </>
   )
