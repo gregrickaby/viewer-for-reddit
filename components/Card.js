8b1d1e52136a7b0366bbfe1e3f0f29300a0f6222
Greg Rickaby <gregrickaby@gmail.com>
2022-06-01 10:54:14 -0500

    remove react-spring

--- components/Card.js
+++ components/Card.js
@@ -1,29 +1,52 @@
 import PropTypes from 'prop-types'
 import cleanIframe from '@/functions/cleanIframe'
-import {useSpring, animated} from 'react-spring'
 
 export default function Card(props) {
-  const spring = useSpring({to: {opacity: 1}, from: {opacity: 0}})
   return (
-    <animated.article style={spring}>
-      <div className="overflow-hidden">
-        {(() => {
-          const [source] = props.images || []
-          switch (props?.type) {
-            case 'image':
-              return (
-                <a href={props?.permalink} aria-label={props?.title}>
-                  <img
-                    alt={props?.title}
-                    className="card-image"
-                    height={source?.source?.height}
-                    loading="lazy"
-                    src={props?.url}
-                    width={source?.source?.width}
-                  />
-                </a>
-              )
-            case 'hosted:video':
+    <div className="overflow-hidden pb-4">
+      {(() => {
+        const [source] = props.images || []
+        switch (props?.type) {
+          case 'image':
+            return (
+              <a href={props?.permalink} aria-label={props?.title}>
+                <img
+                  alt={props?.title}
+                  className="card-image"
+                  height={source?.source?.height}
+                  loading="lazy"
+                  src={props?.url}
+                  width={source?.source?.width}
+                />
+              </a>
+            )
+          case 'hosted:video':
+            return (
+              <video
+                className="aspect-video w-full"
+                autoPlay
+                controls
+                loop
+                muted
+                playsInline
+                src={props?.secure_media?.reddit_video?.fallback_url}
+              />
+            )
+          case 'rich:video':
+            return (
+              <a
+                aria-label={props?.title}
+                dangerouslySetInnerHTML={{
+                  __html: cleanIframe({
+                    html: props?.media?.oembed?.html
+                  })
+                }}
+                href={props?.url}
+              />
+            )
+          case 'link':
+            // Search for .gifv....
+            if (props?.url.includes('gifv')) {
               return (
                 <video
                   className="aspect-video w-full"
@@ -32,45 +55,18 @@ export default function Card(props) {
                   loop
                   muted
                   playsInline
-                  src={props?.secure_media?.reddit_video?.fallback_url}
-                />
+                  src={props?.url.replace('.gifv', '.mp4')} // Replace .gifv with .mp4.
+                ></video>
               )
-            case 'rich:video':
-              return (
-                <a
-                  aria-label={props?.title}
-                  dangerouslySetInnerHTML={{
-                    __html: cleanIframe({
-                      html: props?.media?.oembed?.html
-                    })
-                  }}
-                  href={props?.url}
-                />
-              )
-            case 'link':
-              // Search for .gifv....
-              if (props?.url.includes('gifv')) {
-                return (
-                  <video
-                    className="aspect-video w-full"
-                    autoPlay
-                    controls
-                    loop
-                    muted
-                    playsInline
-                    src={props?.url.replace('.gifv', '.mp4')} // Replace .gifv with .mp4.
-                  ></video>
-                )
-              } else {
-                // No media? Return blank.
-                return <></>
-              }
-            default:
-              break
-          }
-        })()}
-      </div>
-    </animated.article>
+            } else {
+              // No media? Return blank.
+              return <></>
+            }
+          default:
+            break
+        }
+      })()}
+    </div>
   )
 }
 
