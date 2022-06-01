6e43819b1d5c493d7902017d6856e3b34dffaec5
Greg Rickaby <gregrickaby@gmail.com>
2022-06-01 10:54:43 -0500

    add more bones

--- components/Skeleton.js
+++ components/Skeleton.js
@@ -2,7 +2,18 @@ export default function Skeleton() {
   return (
     <>
       <h2 className="sr-only">Loading...</h2>
-      <div className="grid grid-cols-1 place-items-center gap-4 lg:grid-cols-2">
+      <div className="grid grid-cols-1 place-items-center gap-4 lg:grid-cols-3">
+        <div className="skeleton-bone"></div>
+        <div className="skeleton-bone"></div>
+        <div className="skeleton-bone"></div>
+        <div className="skeleton-bone"></div>
+        <div className="skeleton-bone"></div>
+        <div className="skeleton-bone"></div>
+        <div className="skeleton-bone"></div>
+        <div className="skeleton-bone"></div>
+        <div className="skeleton-bone"></div>
+        <div className="skeleton-bone"></div>
+        <div className="skeleton-bone"></div>
         <div className="skeleton-bone"></div>
         <div className="skeleton-bone"></div>
         <div className="skeleton-bone"></div>
