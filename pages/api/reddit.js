2ffeff9b2b31694f10c943509992188866430dd7
Greg Rickaby <gregrickaby@gmail.com>
2022-08-04 18:25:08 -0500

    move post shape. use raw_json. use optimized media

--- pages/api/reddit.js
+++ pages/api/reddit.js
@@ -10,25 +10,10 @@ export default async function reddit(req, res) {
   const sub = req.query.sub ? req.query.sub : 'itookapicture'
   const limit = req.query.limit ? req.query.limit : '24'
 
-  // Format the shape of the post data.
-  function postShape(post) {
-    return {
-      images: post.preview.images,
-      media: post.media,
-      permalink: `https://www.reddit.com${post.permalink}`,
-      secure_media: post.secure_media,
-      subreddit: `https://www.reddit.com/${post.subreddit_name_prefixed}`,
-      thumbnail: post.thumbnail,
-      title: post.title,
-      type: post.post_hint,
-      url: post.url
-    }
-  }
-
   try {
     // Attempt to fetch posts.
     const response = await fetch(
-      `https://oauth.reddit.com/r/${sub}/${sort}/.json?limit=${limit}&after=${after}`,
+      `https://oauth.reddit.com/r/${sub}/${sort}/.json?limit=${limit}&after=${after}&raw_json=1`,
       {
         headers: {
           Authorization: `Bearer: ${process.env.REDDIT_ACCESS_TOKEN}`
@@ -66,8 +51,20 @@ export default async function reddit(req, res) {
 
     // Finally, send the data.
     res.status(200).json({
-      posts: postsContainImage.map((post) => postShape(post?.data)),
-      after: json?.data?.after
+      posts: postsContainImage.map((post) => ({
+        images: post.data.preview.images[0].resolutions.pop(),
+        media: post.data.media,
+        permalink: `https://www.reddit.com${post.data.permalink}`,
+        secure_media: post.secure_media,
+        subreddit: `https://www.reddit.com/${post.data.subreddit_name_prefixed}`,
+        thumbnail: post.data.thumbnail,
+        title: post.data.title,
+        type: post.data.post_hint,
+        ups: post.data.ups,
+        url: post.data.url
+      })),
+      after: json?.data?.after,
+      all: json?.data
     })
   } catch (error) {
     // Issue? Leave a message and bail.
