b4b609e6c1d93e36ecd9c05abff0ec466e0aa73c
Greg Rickaby <gregrickaby@gmail.com>
2022-08-22 14:27:40 -0500

    display subs

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,22 @@
 import {useSession, signIn, signOut} from 'next-auth/react'
+import {useSubs} from '~/lib/helpers'
 
 export default function Header() {
   const {data: session} = useSession()
+  const {subs, isLoading, isError} = useSubs(session?.user?.name ? true : false)
+
   if (session) {
     return (
       <>
-        Signed in as {session.user.email} <br />
+        Hello {session.user.name} <br />
+        <pre>{JSON.stringify(session, null, 2)}</pre>
         <button onClick={() => signOut()}>Sign out</button>
+        {!isLoading && !isError && <pre>{JSON.stringify(subs, null, 2)}</pre>}
       </>
     )
   }
   return (
     <>
-      Not signed in <br />
       <button onClick={() => signIn()}>Sign in</button>
     </>
   )
