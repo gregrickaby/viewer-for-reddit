7930682f7ed76fe21240a99f49804adca2d62790
Greg Rickaby <gregrickaby@gmail.com>
2022-08-22 14:26:58 -0500

    pass token down to session. add docblocks

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,52 @@
-import NextAuth from 'next-auth'
 import RedditProvider from 'next-auth/providers/reddit'
+import NextAuth from 'next-auth'
+import type {NextAuthOptions} from 'next-auth'
 
-export default NextAuth({
+/**
+ * Set authorization options for Reddit Provider.
+ *
+ * @see https://next-auth.js.org/providers/reddit
+ * @see https://next-auth.js.org/configuration/providers/oauth
+ * @see https://next-auth.js.org/configuration/callbacks
+ * @see https://github.com/reddit-archive/reddit/wiki/OAuth2#authorization
+ *
+ */
+export const authOptions: NextAuthOptions = {
   providers: [
     RedditProvider({
       clientId: process.env.MANTINE_REDDIT_CLIENT_ID,
-      clientSecret: process.env.MANTINE_REDDIT_CLIENT_SECRET
+      clientSecret: process.env.MANTINE_REDDIT_CLIENT_SECRET,
+      profile(profile) {
+        return {
+          id: profile.id,
+          name: profile.name,
+          image: profile.snoovatar_img
+        }
+      },
+      authorization: {
+        params: {
+          duration: 'permanent',
+          scope: 'identity mysubreddits history save vote'
+        }
+      }
     })
-  ]
-})
+  ],
+  callbacks: {
+    async jwt({token, account}) {
+      if (account) {
+        // Append Reddit access token to the JWT session.
+        token.accessToken = account.access_token
+      }
+      return token
+    },
+    async session({session, token}) {
+      if (token) {
+        // Append the access token to the current session.
+        session.accessToken = token.accessToken
+      }
+      return session
+    }
+  }
+}
+
+export default NextAuth(authOptions)
