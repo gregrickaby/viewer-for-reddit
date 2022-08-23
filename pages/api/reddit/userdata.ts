8fbe4ccb6241e61f1c625bfd82df464004f3a5ff
Greg Rickaby <gregrickaby@gmail.com>
2022-08-23 16:44:21 -0500

    formatting. add comments.

--- pages/api/reddit/userdata.ts
+++ pages/api/reddit/userdata.ts
@@ -1,73 +1,62 @@
-import type {NextApiRequest, NextApiResponse} from 'next'
-import {authOptions} from '../auth/[...nextauth]'
-import {unstable_getServerSession} from 'next-auth/next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { unstable_getServerSession } from 'next-auth/next';
+import { authOptions } from '../auth/[...nextauth]';
 
 /**
  * Get user's stuff from Reddit.
  *
  * @see https://www.reddit.com/dev/api/oauth
  */
-export default async function userdata(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const session = await unstable_getServerSession(req, res, authOptions)
+export default async function userdata(req: NextApiRequest, res: NextApiResponse) {
+  // Get session data.
+  const session = await unstable_getServerSession(req, res, authOptions);
 
   // No session? Bail...
   if (!session) {
-    res.status(401).json({message: 'You must be logged in.'})
+    res.status(401).json({ message: 'You must be logged in.' });
   }
 
   // No access token? Bail...
   if (!session.accessToken) {
-    res.status(401).json({message: 'Missing access token.'})
+    res.status(401).json({ message: 'Missing access token.' });
   }
 
   try {
     // Try and fetch user's subs.
-    const subResponse = await fetch(
-      'https://oauth.reddit.com/subreddits/mine/subscriber',
-      {
-        headers: {
-          authorization: `Bearer ${session.accessToken}`
-        }
-      }
-    )
-    const subJson = await subResponse.json()
-    const subs = subJson.data.children.map((sub) => sub.data.display_name)
+    const subResponse = await fetch('https://oauth.reddit.com/subreddits/mine/subscriber', {
+      headers: {
+        authorization: `Bearer ${session.accessToken}`,
+      },
+    });
+    const subJson = await subResponse.json();
+    const subs = subJson.data.children.map((sub) => sub.data.display_name);
 
     // Try and fetch user's prefs.
-    const prefsResponse = await fetch(
-      'https://oauth.reddit.com/api/v1/me/prefs',
-      {
-        headers: {
-          authorization: `Bearer ${session.accessToken}`
-        }
-      }
-    )
-    const prefs = await prefsResponse.json()
+    const prefsResponse = await fetch('https://oauth.reddit.com/api/v1/me/prefs', {
+      headers: {
+        authorization: `Bearer ${session.accessToken}`,
+      },
+    });
+    const prefs = await prefsResponse.json();
 
     // Try and fetch user's friends.
-    const friendsResponse = await fetch(
-      'https://oauth.reddit.com/prefs/friends',
-      {
-        headers: {
-          authorization: `Bearer ${session.accessToken}`
-        }
-      }
-    )
-    const friends = await friendsResponse.json()
+    const friendsResponse = await fetch('https://oauth.reddit.com/prefs/friends', {
+      headers: {
+        authorization: `Bearer ${session.accessToken}`,
+      },
+    });
+    const friends = await friendsResponse.json();
 
     // Try and fetch user's multis.
     const response = await fetch('https://oauth.reddit.com/api/multi/mine', {
       headers: {
-        authorization: `Bearer ${session.accessToken}`
-      }
-    })
-    const multis = await response.json()
+        authorization: `Bearer ${session.accessToken}`,
+      },
+    });
+    const multis = await response.json();
 
-    res.status(200).json({subs, prefs, friends, multis})
+    res.status(200).json({ subs, prefs, friends, multis });
   } catch (error) {
-    res.status(500).json({message: `${error}`})
+    res.status(500).json({ message: `${error}` });
   }
 }
