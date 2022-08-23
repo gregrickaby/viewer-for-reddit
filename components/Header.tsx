684cd8acd273de78845c9ec49f22a35d444fadf3
Greg Rickaby <gregrickaby@gmail.com>
2022-08-22 19:35:48 -0500

    testing userdata

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,35 +1,21 @@
-import {useSession, signIn, signOut} from 'next-auth/react'
-import {useState} from 'react'
+import {signIn, signOut, useSession} from 'next-auth/react'
+import {useRedditContext} from '~/components/RedditProvider'
 
 export default function Header() {
   const {data: session} = useSession()
-  const [subs, setSubs] = useState()
+  const {isError, isLoading, userData} = useRedditContext()
 
-  async function getSubs() {
-    const response = await fetch(
-      'https://oauth.reddit.com/subreddits/mine/subscriber/',
-      {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer: ${session.accessToken}`
-        }
-      }
-    )
-    const subs = await response.json()
-    setSubs(subs)
-  }
-
-  if (session) {
+  if (session && !isError && !isLoading) {
     return (
       <>
         Hello {session.user.name} <br />
         <pre>{JSON.stringify(session, null, 2)}</pre>
         <button onClick={() => signOut()}>Sign out</button>
-        <button onClick={() => getSubs()}>Get Subs</button>
-        {<pre>{JSON.stringify(subs, null, 2)}</pre>}
+        {userData && <pre>{JSON.stringify(userData, null, 2)}</pre>}
       </>
     )
   }
+
   return (
     <>
       <button onClick={() => signIn()}>Sign in</button>
