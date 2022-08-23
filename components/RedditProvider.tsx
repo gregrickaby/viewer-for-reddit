b5d5914379d2548754e01d2dc170e7d9a9287770
Greg Rickaby <gregrickaby@gmail.com>
2022-08-23 16:44:47 -0500

    refactor to store userData() in localstorage

--- components/RedditProvider.tsx
+++ components/RedditProvider.tsx
@@ -1,42 +1,44 @@
-import {createContext, useContext} from 'react'
-import {useUserData} from '~/lib/helpers'
-import {useSession} from 'next-auth/react'
-import {ChildrenProps} from '~/lib/types'
+import { createContext, useContext, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useLocalStorage } from '@mantine/hooks';
+import { useUserData } from '~/lib/helpers';
+import { ChildrenProps } from '~/lib/types';
 
 export interface RedditProviderProps {
-  isError: boolean
-  isLoading: boolean
-  userData: {
-    [key: string]: any
-  }
+  app: any;
+  setApp: (app: {}) => void;
 }
 
 // Create the RedditContext.
-const RedditContext = createContext({} as RedditProviderProps)
+const RedditContext = createContext({} as RedditProviderProps);
 
 // Create useRedditContext hook.
-export const useRedditContext = () => useContext(RedditContext)
+export const useRedditContext = () => useContext(RedditContext);
 
 /**
  * RedditProvider component.
  *
  * This component is used to hold global state and provide it to child components.
  */
-export default function RedditProvider({children}: ChildrenProps) {
-  const {data: session} = useSession()
-  const {userData, isLoading, isError} = useUserData(
-    session?.user?.name ? true : false
-  )
+export default function RedditProvider({ children }: ChildrenProps) {
+  const { data: session } = useSession();
+  const { userData } = useUserData(!!session?.user?.name);
+  const [app, setApp] = useLocalStorage({ key: 'riv-app' });
 
   const providerValues = {
-    isError,
-    isLoading,
-    userData
-  }
+    app,
+    setApp,
+  };
+
+  useEffect(() => {
+    if (userData) {
+      setApp(userData);
+    }
+  }, [userData]);
 
   return (
     <RedditContext.Provider value={providerValues as RedditProviderProps}>
       {children}
     </RedditContext.Provider>
-  )
+  );
 }
