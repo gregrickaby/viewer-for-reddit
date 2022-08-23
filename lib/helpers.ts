4559c6dfa9914b77ef3cd6af7cc0c391b96e6d9f
Greg Rickaby <gregrickaby@gmail.com>
2022-08-23 16:45:18 -0500

    add more helper functions. formatting.

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,18 +1,19 @@
-import useSWR from 'swr'
+import useSWR from 'swr';
+import { signOut } from 'next-auth/react';
 
 /**
  * Generic fetcher for useSWR() package.
  */
 export async function fetcher(url: string) {
-  return await fetch(url).then((res) => res.json())
+  return fetch(url).then((res) => res.json());
 }
 
 /**
  * Grab the src of an iframe and replace it with something less terrible.
  */
-export function cleanIframe({html}): string {
+export function cleanIframe({ html }): string {
   // Grab the src URL.
-  const source = html.match(/(src="([^"]+)")/gi)
+  const source = html.match(/(src="([^"]+)")/gi);
 
   return `<iframe
       ${source}
@@ -21,24 +22,43 @@ export function cleanIframe({html}): string {
       referrerpolicy="no-referrer"
       sandbox="allow-scripts allow-same-origin allow-presentation"
       title="iframe"
-    />`
+    />`;
+}
+
+export function logOut(): void {
+  localStorage.removeItem('riv-app');
+  localStorage.removeItem('nextauth.message');
+  signOut();
 }
 
 /**
- * Grab all the user's data from the API.
+ * Fetch a subreddit and return the data.
+ */
+export function useSubreddit(shouldFetch: boolean) {
+  const { data, error } = useSWR(shouldFetch ? '/api/reddit/subreddit' : null, fetcher);
+
+  return {
+    posts: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
+}
+
+/**
+ * Fetch user's data from the API.
  */
 export function useUserData(shouldFetch: boolean) {
-  const {data, error} = useSWR(
-    shouldFetch ? '/api/reddit/userdata' : null,
-    fetcher,
-    {
-      revalidateOnFocus: false
-    }
-  )
+  const { data, error } = useSWR(shouldFetch ? '/api/reddit/userdata' : null, fetcher, {
+    revalidateOnFocus: false,
+  });
+
+  if (error) {
+    return {
+      userData: null,
+    };
+  }
 
   return {
     userData: data,
-    isLoading: !error && !data,
-    isError: error
-  }
+  };
 }
