70e4babb921652d336307d7bec1c555ae6d91a48
Greg Rickaby <gregrickaby@gmail.com>
2022-08-27 09:14:46 -0500

    add NotFound component

---
+++ components/NotFound.tsx
@@ -0,0 +1,18 @@
+import { Stack, Text } from '@mantine/core';
+import Image from 'next/future/image';
+import img from '../public/obi-404.webp';
+
+/**
+ * Not Found component.
+ */
+export default function NotFound() {
+  return (
+    <Stack>
+      <Text>
+        Either the Reddit API is down, this content doesn&apos;t exist, or something else is wrong.
+        Please try again.
+      </Text>
+      <Image alt="404 not found" src={img} />
+    </Stack>
+  );
+}
