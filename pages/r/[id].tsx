cc0ac69f73387368df8712b1d1197340ea4892ae
Greg Rickaby <gregrickaby@gmail.com>
2022-08-27 09:14:20 -0500

    add subreddit (/r/) route

---
+++ pages/r/[id].tsx
@@ -0,0 +1,53 @@
+import { Masonry } from 'masonic';
+import { GetServerSideProps } from 'next';
+import Layout from '~/components/Layout';
+import { MasonryCard } from '~/components/MasonryCard';
+import NotFound from '~/components/NotFound';
+import { useSubreddit } from '~/lib/helpers';
+
+export interface SubredditProps {
+  subreddit: string;
+}
+
+/**
+ * Subreddit component.
+ */
+export default function Subreddit({ subreddit }) {
+  const { posts, isLoading } = useSubreddit({ subreddit, shouldFetch: true });
+
+  // Loading?
+  if (isLoading) {
+    return <Layout>Loading {subreddit}...</Layout>;
+  }
+
+  // No posts? Bail...
+  if (!posts && posts.posts.length === 0) {
+    return (
+      <Layout>
+        <NotFound />
+      </Layout>
+    );
+  }
+
+  // Finally, render posts.
+  return (
+    <Layout>
+      <Masonry
+        items={posts.posts}
+        render={MasonryCard}
+        columnGutter={16}
+        columnWidth={300}
+        overscanBy={1}
+      />
+    </Layout>
+  );
+}
+
+/**
+ * Get the subreddit from the URL.
+ */
+export const getServerSideProps: GetServerSideProps = async (context) => ({
+  props: {
+    subreddit: context.query.id,
+  },
+});
