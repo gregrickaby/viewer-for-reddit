b50d09083c9edeccc15a078f2b76ca7fedf11e9c
Greg Rickaby <gregrickaby@gmail.com>
2022-08-28 19:56:45 -0500

    support multis

---
+++ pages/api/multis.ts
@@ -0,0 +1,40 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { unstable_getServerSession } from 'next-auth/next';
+import { postResponseShaper } from '~/lib/helpers';
+import { authOptions } from './auth/[...nextauth]';
+
+/**
+ * Query a multi reddit.
+ *
+ * This query must be available to non-authenticated users.
+ *
+ * @see https://www.reddit.com/dev/api
+ */
+export default async function multis(req: NextApiRequest, res: NextApiResponse) {
+  // Get session data.
+  const session = await unstable_getServerSession(req, res, authOptions);
+
+  // Parse query and set defaults.
+  const after = req.query.after ? req.query.after : '';
+  const sort = req.query.sort ? req.query.sort : 'hot';
+  const feed = req.query.feed ? req.query.feed : '';
+  const limit = req.query.limit ? req.query.limit : '24';
+
+  /**
+   * Fetch a multi.
+   */
+  try {
+    const response = await fetch(
+      `https://oauth.reddit.com/user/${session.user.name}/m/${feed}/${sort}/.json?limit=${limit}&after=${after}&raw_json=1`,
+      {
+        headers: {
+          authorization: `Bearer ${session.accessToken}`,
+        },
+      }
+    );
+    const json = await response.json();
+    res.status(200).json(postResponseShaper(json));
+  } catch (error) {
+    res.status(500).json({ message: `${error}` });
+  }
+}
