45e7072c3a655828fbb7758378d66519367e7722
Greg Rickaby <greg.rickaby@americaneagle.com>
2022-09-12 10:51:26 -0500

    dependency bump

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    images: {
-      allowFutureImage: true,
-    },
-  },
+module.exports = {
   images: {
     formats: ['image/avif', 'image/webp'],
   },
   reactStrictMode: true,
   swcMinify: true,
 };
-
-module.exports = nextConfig;
