adbba5378a80398846804805ae26b74063159c2c
Greg Rickaby <gregrickaby@gmail.com>
2022-10-03 17:57:08 -0500

    style skeleton

--- components/SkeletonWrapper.tsx
+++ components/SkeletonWrapper.tsx
@@ -1,35 +1,46 @@
-import {Skeleton} from '@mantine/core'
+import {createStyles, Skeleton} from '@mantine/core'
+
+const useStyles = createStyles((theme) => ({
+  wrapper: {
+    alignItems: 'start',
+    display: 'grid',
+    gridGap: theme.spacing.md,
+    gridTemplateColumns: `repeat(auto-fit, 373px)`,
+    justifyContent: 'center'
+  }
+}))
 
 /**
  * Skeleton wrapper component.
  */
 export default function SkeletonWrapper() {
+  const {classes} = useStyles()
   return (
-    <>
-      <Skeleton height={640} mt={6} width={480} />
-      <Skeleton height={640} mt={6} width={480} />
-      <Skeleton height={640} mt={6} width={480} />
-      <Skeleton height={640} mt={6} width={480} />
-      <Skeleton height={640} mt={6} width={480} />
-      <Skeleton height={640} mt={6} width={480} />
-      <Skeleton height={640} mt={6} width={480} />
-      <Skeleton height={640} mt={6} width={480} />
-      <Skeleton height={640} mt={6} width={480} />
-      <Skeleton height={640} mt={6} width={480} />
-      <Skeleton height={640} mt={6} width={480} />
-      <Skeleton height={640} mt={6} width={480} />
-      <Skeleton height={640} mt={6} width={480} />
-      <Skeleton height={640} mt={6} width={480} />
-      <Skeleton height={640} mt={6} width={480} />
-      <Skeleton height={640} mt={6} width={480} />
-      <Skeleton height={640} mt={6} width={480} />
-      <Skeleton height={640} mt={6} width={480} />
-      <Skeleton height={640} mt={6} width={480} />
-      <Skeleton height={640} mt={6} width={480} />
-      <Skeleton height={640} mt={6} width={480} />
-      <Skeleton height={640} mt={6} width={480} />
-      <Skeleton height={640} mt={6} width={480} />
-      <Skeleton height={640} mt={6} width={480} />
-    </>
+    <div className={classes.wrapper}>
+      <Skeleton height={373} width={373} />
+      <Skeleton height={373} width={373} />
+      <Skeleton height={373} width={373} />
+      <Skeleton height={373} width={373} />
+      <Skeleton height={373} width={373} />
+      <Skeleton height={373} width={373} />
+      <Skeleton height={373} width={373} />
+      <Skeleton height={373} width={373} />
+      <Skeleton height={373} width={373} />
+      <Skeleton height={373} width={373} />
+      <Skeleton height={373} width={373} />
+      <Skeleton height={373} width={373} />
+      <Skeleton height={373} width={373} />
+      <Skeleton height={373} width={373} />
+      <Skeleton height={373} width={373} />
+      <Skeleton height={373} width={373} />
+      <Skeleton height={373} width={373} />
+      <Skeleton height={373} width={373} />
+      <Skeleton height={373} width={373} />
+      <Skeleton height={373} width={373} />
+      <Skeleton height={373} width={373} />
+      <Skeleton height={373} width={373} />
+      <Skeleton height={373} width={373} />
+      <Skeleton height={373} width={373} />
+    </div>
   )
 }
