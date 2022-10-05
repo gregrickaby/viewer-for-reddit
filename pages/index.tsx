146ae134fc5160afade3555e035e85fed5c5d53d
Greg Rickaby <gregrickaby@gmail.com>
2022-10-05 13:57:48 -0500

    use new components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import {createStyles} from '@mantine/core'
-import {IconBrandGithub} from '@tabler/icons'
-import Head from 'next/head'
 import BackToTop from '~/components/BackToTop'
+import Footer from '~/components/Footer'
+import Header from '~/components/Header'
+import Meta from '~/components/Meta'
 import Results from '~/components/Results'
 import Search from '~/components/Search'
+import Settings from '~/components/Settings'
 import Sort from '~/components/Sort'
-import config from '~/lib/config'
 
 const useStyles = createStyles((theme) => ({
   container: {
@@ -16,40 +17,13 @@ const useStyles = createStyles((theme) => ({
       padding: theme.spacing.xl
     }
   },
-  header: {
-    alignItems: 'center',
-    display: 'flex',
-    justifyContent: 'space-between',
-    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
-      flexDirection: 'column',
-      textAlign: 'center'
-    }
-  },
-  title: {
-    fontSize: theme.fontSizes.xl,
-    margin: 0,
-    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
-      lineHeight: 1
-    }
-  },
-  controls: {
+  search: {
     display: 'flex',
     gap: theme.spacing.md,
     marginBottom: theme.spacing.xl
   },
   main: {
     minHeight: '100vh'
-  },
-  footer: {
-    alignItems: 'center',
-    display: 'flex',
-    gap: theme.spacing.md,
-    justifyContent: 'center',
-    textAlign: 'center',
-
-    a: {
-      color: theme.colors.dark[0]
-    }
   }
 }))
 
@@ -60,43 +34,18 @@ export default function Homepage() {
   const {classes} = useStyles()
   return (
     <>
-      <Head>
-        <title>{config?.siteTitle}</title>
-        <meta name="description" content={config?.siteDescription} />
-      </Head>
+      <Meta />
       <div className={classes.container}>
-        <header className={classes.header}>
-          <h1 className={classes.title}>{config.siteTitle}</h1>
-          <p>{config.siteDescription}</p>
-        </header>
+        <Header />
         <main className={classes.main}>
-          <div className={classes.controls}>
+          <div className={classes.search}>
             <Search />
             <Sort />
+            <Settings />
           </div>
           <Results />
         </main>
-        <footer className={classes.footer}>
-          <p>
-            website by{' '}
-            <a
-              href={config.authorUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {config.siteAuthor}
-            </a>
-          </p>
-          <p>
-            <a
-              href="https://github.com/gregrickaby/reddit-image-viewer"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <IconBrandGithub />
-            </a>
-          </p>
-        </footer>
+        <Footer />
       </div>
       <BackToTop />
     </>
