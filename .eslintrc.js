dd67d057e4fabad8268565c8d85218c8008595cf
Greg Rickaby <gregrickaby@gmail.com>
2022-10-07 07:33:32 -0500

    introduce func-style rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['next/core-web-vitals', 'prettier'],
   rules: {
     '@next/next/no-img-element': 'off',
+    'func-style': ['error', 'declaration'],
     'no-console': ['error', {allow: ['warn', 'error']}]
   }
 }
