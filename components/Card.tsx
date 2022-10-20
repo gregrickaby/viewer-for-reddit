e28fab504aac8f2ae18b9fc391d912617f404c1b
Greg Rickaby <gregrickaby@gmail.com>
2022-10-20 09:49:43 -0500

    remove extra link. update hover styles.

--- components/Card.tsx
+++ components/Card.tsx
@@ -15,7 +15,8 @@ const useStyles = createStyles((theme) => ({
     transition: 'all 0.25s ease-in',
 
     ':hover': {
-      filter: 'brightness(1.25)'
+      cursor: 'pointer',
+      filter: 'brightness(1)'
     }
   },
   blurred: {
@@ -36,91 +37,90 @@ export default function Card(props: Post) {
           case 'image':
             return (
               <>
-              <a href={props?.permalink}>
-                <img
-                  alt={props?.title}
-                  className={cx(
-                    classes.media,
-                    props.over_18 && blurNSFW ? classes.blurred : null
-                  )}
-                  data-hint="image"
-                  height={props?.images?.height}
-                  loading="lazy"
-                  src={props?.images?.url}
-                  width={props?.images?.width}
-                />
-              </a>
-                <a href={props?.permalink}>{props.title}</a>
+                <a href={props?.permalink}>
+                  <img
+                    alt={props?.title}
+                    className={cx(
+                      classes.media,
+                      props.over_18 && blurNSFW ? classes.blurred : null
+                    )}
+                    data-hint="image"
+                    height={props?.images?.height}
+                    loading="lazy"
+                    src={props?.images?.url}
+                    width={props?.images?.width}
+                  />
+                </a>
               </>
             )
           case 'hosted:video':
             return (
               <>
-              <HlsPlayer
-                className={cx(
-                  classes.media,
-                  props.over_18 && blurNSFW ? classes.blurred : null
-                )}
-                src={props?.media?.reddit_video?.hls_url}
-                controls
-                crossOrigin="anonymous"
-                dataHint="hosted:video"
-                height={props?.media?.reddit_video?.height}
-                playsInline
-                preload="metadata"
-                width={props?.media?.reddit_video?.width}
-              >
-                <source
-                  src={props?.media?.reddit_video?.fallback_url}
-                  type="video/mp4"
-                />
-              </HlsPlayer>
+                <HlsPlayer
+                  className={cx(
+                    classes.media,
+                    props.over_18 && blurNSFW ? classes.blurred : null
+                  )}
+                  src={props?.media?.reddit_video?.hls_url}
+                  controls
+                  crossOrigin="anonymous"
+                  dataHint="hosted:video"
+                  height={props?.media?.reddit_video?.height}
+                  playsInline
+                  preload="metadata"
+                  width={props?.media?.reddit_video?.width}
+                >
+                  <source
+                    src={props?.media?.reddit_video?.fallback_url}
+                    type="video/mp4"
+                  />
+                </HlsPlayer>
                 <a href={props?.permalink}>{props.title}</a>
               </>
             )
           case 'rich:video':
             return props?.video_preview ? (
               <>
-              <HlsPlayer
-                className={cx(
-                  classes.media,
-                  props.over_18 && blurNSFW ? classes.blurred : null
-                )}
-                src={props?.video_preview?.hls_url}
-                controls
-                crossOrigin="anonymous"
-                dataHint="rich:video"
-                height={props?.video_preview?.height}
-                muted
-                playsInline
-                preload="metadata"
-                width={props?.video_preview?.width}
-              >
-                <source
-                  src={props?.video_preview?.fallback_url}
-                  type="video/mp4"
-                />
-              </HlsPlayer>
+                <HlsPlayer
+                  className={cx(
+                    classes.media,
+                    props.over_18 && blurNSFW ? classes.blurred : null
+                  )}
+                  src={props?.video_preview?.hls_url}
+                  controls
+                  crossOrigin="anonymous"
+                  dataHint="rich:video"
+                  height={props?.video_preview?.height}
+                  muted
+                  playsInline
+                  preload="metadata"
+                  width={props?.video_preview?.width}
+                >
+                  <source
+                    src={props?.video_preview?.fallback_url}
+                    type="video/mp4"
+                  />
+                </HlsPlayer>
                 <a href={props?.permalink}>{props.title}</a>
               </>
             ) : (
               <>
-              <div
-                style={{
-                  height: props?.secure_media_embed?.height,
-                  width: props?.secure_media_embed?.width
-                }}
-              >
-                <iframe
-                  allow="fullscreen"
-                  loading="lazy"
-                  referrerPolicy="no-referrer"
-                  sandbox="allow-scripts allow-same-origin allow-presentation"
-                  src={props?.secure_media_embed?.media_domain_url}
-                  style={{border: 'none', height: '100%', width: '100%'}}
-                  title="iframe"
-                />
-              </div>
+                <div
+                  style={{
+                    height: props?.secure_media_embed?.height,
+                    width: props?.secure_media_embed?.width
+                  }}
+                >
+                  <iframe
+                    allow="fullscreen"
+                    loading="lazy"
+                    referrerPolicy="no-referrer"
+                    sandbox="allow-scripts allow-same-origin allow-presentation"
+                    src={props?.secure_media_embed?.media_domain_url}
+                    style={{border: 'none', height: '100%', width: '100%'}}
+                    title="iframe"
+                  />
+                </div>
                 <a href={props?.permalink}>{props.title}</a>
               </>
             )
@@ -129,22 +129,22 @@ export default function Card(props: Post) {
             if (props?.url.includes('gifv')) {
               return (
                 <>
-                <HlsPlayer
-                  className={cx(
-                    (classes.media,
-                    {[classes.blurred]: props.over_18 && blurNSFW})
-                  )}
-                  controls
-                  dataHint="link"
-                  muted
-                  playsInline
-                  preload="metadata"
-                >
-                  <source
-                    src={props?.url.replace('.gifv', '.mp4')}
-                    type="video/mp4"
-                  />
-                </HlsPlayer>
+                  <HlsPlayer
+                    className={cx(
+                      (classes.media,
+                      {[classes.blurred]: props.over_18 && blurNSFW})
+                    )}
+                    controls
+                    dataHint="link"
+                    muted
+                    playsInline
+                    preload="metadata"
+                  >
+                    <source
+                      src={props?.url.replace('.gifv', '.mp4')}
+                      type="video/mp4"
+                    />
+                  </HlsPlayer>
                   <a href={props?.permalink}>{props.title}</a>
                 </>
               )
