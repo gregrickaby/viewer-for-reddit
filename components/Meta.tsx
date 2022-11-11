3b29fc003d3d3b95fbe950c691504fdcf9cc5fc8
Greg Rickaby <gregrickaby@gmail.com>
2022-11-11 06:52:39 -0600

    update meta tags

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -9,8 +9,9 @@ export default function Meta() {
     <Head>
       <title>{config?.siteTitle}</title>
       <meta name="description" content={config?.siteDescription} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+      <link rel="preconnect" href="//i.imgur.com" crossOrigin="anonymous" />
       <link rel="preconnect" href="//preview.redd.it" crossOrigin="anonymous" />
       <link rel="preconnect" href="//v.redd.it" crossOrigin="anonymous" />
       <link
@@ -18,34 +19,29 @@ export default function Meta() {
         href="//external-preview.redd.it"
         crossOrigin="anonymous"
       />
+
       <link
         as="fetch"
         rel="preload"
         href="/api/reddit?sub=itookapicture&amp;sort=hot&amp;limit=24&amp;after="
         crossOrigin="same-origin"
       />
-      <link rel="canonical" href="https://reddit-image-viewer.vercel.app/" />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <link rel="apple-touch-icon" href="/favicon/icon.png" />
-      <link rel="icon" href="/favicon/icon.png" sizes="192x192" />
 
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content={config?.siteUrl} />
       <meta property="og:title" content={config?.siteTitle} />
       <meta property="og:description" content={config?.siteDescription} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={config?.siteUrl} />
       <meta
         property="og:image"
         content={`${config?.siteUrl}social-share.jpg`}
       />
 
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content={config?.siteUrl} />
-      <meta property="twitter:title" content={config?.siteTitle} />
-      <meta property="twitter:description" content={config?.siteDescription} />
-      <meta
-        property="twitter:image"
-        content={`${config?.siteUrl}social-share.jpg`}
-      />
+      <link rel="canonical" href={config?.siteUrl} />
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="icon" href="/icons/icon.png" sizes="192x192" />
+      <link rel="apple-touch-icon" href="/icons/icon.png" />
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#ffffff" />
 
       <meta
         name="google-site-verification"
