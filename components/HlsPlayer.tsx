547ba52f7742acf1580bea6aa1be924f93454c17
Greg Rickaby <gregrickaby@gmail.com>
2023-02-24 10:06:48 -0600

    add comments. reverse if/else

--- components/HlsPlayer.tsx
+++ components/HlsPlayer.tsx
@@ -1,6 +1,7 @@
 import Hls from 'hls.js'
 import {LegacyRef, useEffect, useRef, VideoHTMLAttributes} from 'react'
 
+// Allow the data-hint prop on the VideoHTMLAttributes interface.
 export interface HlsPlayerProps
   extends Omit<VideoHTMLAttributes<HTMLVideoElement>, 'data-hint'> {
   dataHint?: string
@@ -12,18 +13,27 @@ export interface HlsPlayerProps
  * @see https://github.com/video-dev/hls.js/
  */
 export default function HlsPlayer(props: HlsPlayerProps) {
+  // Create a ref to the video element.
   const videoRef = useRef<HTMLMediaElement>(null)
 
+  /**
+   * Initialize Hls.js.
+   *
+   * @see https://github.com/video-dev/hls.js/#embedding-hlsjs
+   */
   useEffect(() => {
     const video = videoRef.current
 
-    if (props.src != undefined && video !== null) {
-      if (Hls.isSupported()) {
+    if (props.src !== undefined && video !== null) {
+      // First check for native browser HLS support.
+      if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        video.src = props.src
+
+        // If no native HLS support, check if HLS.js is supported.
+      } else if (Hls.isSupported()) {
         const hls = new Hls()
         hls.loadSource(props.src)
         hls.attachMedia(video)
-      } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-        video.src = props.src
       }
     }
   }, [props.src, videoRef])
