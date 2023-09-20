74f16464909f4fd23667a883d18060828e388c60
Greg Rickaby <gregrickaby@gmail.com>
2023-09-20 09:42:02 -0500

    check-in <NotFound />

---
+++ app/not-found.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import {Stack} from '@mantine/core'
+import Image from 'next/image'
+import notFound from '../public/not-found.webp'
+
+/**
+ * 404 Not Found component.
+ */
+export default function NotFound() {
+  return (
+    <Stack align="center">
+      <p>
+        Either the Reddit API is down or something else is wrong. Please try
+        your search again.
+      </p>
+      <Image alt="404 not found" priority placeholder="blur" src={notFound} />
+    </Stack>
+  )
+}
