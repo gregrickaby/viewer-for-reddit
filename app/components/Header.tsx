07e082d19c76d1fdc8c20a70449a92947596dd6c
Greg Rickaby <gregrickaby@gmail.com>
2023-09-20 09:06:58 -0500

    initial <Header />

---
+++ app/components/Header.tsx
@@ -0,0 +1,19 @@
+import {Title} from '@mantine/core'
+import classes from '~/components/Header.module.css'
+import config from '~/lib/config'
+
+/**
+ * Header component.
+ */
+export default function Header() {
+  return (
+    <header className={classes.header}>
+      <Title className={classes.title} order={1}>
+        {config.siteName}
+      </Title>
+      <Title className={classes.description} order={2}>
+        {config.siteDescription}
+      </Title>
+    </header>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import Header from '~/components/Header'
+
 export default function HomePage() {
-  return <div>Home page</div>
+  return (
+    <>
+      <Header />
+      <main>
+        <p>Home page</p>
+      </main>
+    </>
+  )
 }
