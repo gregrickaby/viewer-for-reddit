394fda1dda641f6f2fac2a457b5f377079700f4a
Greg Rickaby <gregrickaby@gmail.com>
2023-09-20 10:01:16 -0500

    use reddit provider

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,14 +1,27 @@
+'use client'
+
 import {Title} from '@mantine/core'
 import classes from '~/components/Header.module.css'
+import {useRedditContext} from '~/components/RedditProvider'
 import config from '~/lib/config'
 
 /**
  * Header component.
  */
 export default function Header() {
+  const {setSearchInput, setSubreddit} = useRedditContext()
+
+  /**
+   * Reset the search input and subreddit.
+   */
+  function resetSearch() {
+    setSearchInput(config?.redditApi?.subReddit)
+    setSubreddit(config?.redditApi?.subReddit)
+  }
+
   return (
     <header className={classes.header}>
-      <Title className={classes.title} order={1}>
+      <Title className={classes.title} order={1} onClick={resetSearch}>
         {config.siteName}
       </Title>
       <Title className={classes.description} order={2}>
