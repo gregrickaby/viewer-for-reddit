584ac1b04727584c9008b55ec2295077765c92d3
Greg Rickaby <gregrickaby@gmail.com>
2023-11-26 10:30:28 -0600

    move Meta into metadata()

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,76 @@
+import PreloadResources from '@/components/PreloadResources'
+import RedditProvider from '@/components/RedditProvider'
+import config from '@/lib/config'
+import {theme} from '@/theme'
 import {ColorSchemeScript, MantineProvider} from '@mantine/core'
 import '@mantine/core/styles.css'
-import Meta from '~/components/Meta'
-import RedditProvider from '~/components/RedditProvider'
-import config from '~/lib/config'
-import {theme} from '../theme'
+import {Metadata, Viewport} from 'next'
 
-export const metadata = {
+/**
+ * Setup metadata.
+ *
+ * @see https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+ */
+export const metadata: Metadata = {
+  metadataBase: new URL(config.siteUrl),
   title: `${config.siteName} - ${config.siteDescription}`,
-  description: config.siteDescription
+  description: config.siteDescription,
+  robots: 'follow, index',
+  alternates: {
+    canonical: config.siteUrl
+  },
+  manifest: '/manifest.json',
+  openGraph: {
+    title: config.siteName,
+    description: config.metaDescription,
+    type: 'website',
+    locale: 'en_US',
+    url: config.siteUrl,
+    images: [
+      {
+        url: `${config.siteUrl}social-share.webp`,
+        width: 1200,
+        height: 630,
+        alt: config.siteName
+      }
+    ]
+  },
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/icon.png',
+    shortcut: '/icon.png'
+  },
+  other: {
+    'google-site-verification':
+      process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || ''
+  }
+}
+
+/**
+ * Setup viewport.
+ *
+ * @see https://nextjs.org/docs/app/api-reference/functions/generate-viewport
+ */
+export const viewport: Viewport = {
+  minimumScale: 1,
+  userScalable: false,
+  themeColor: [
+    {media: '(prefers-color-scheme: light)', color: 'white'},
+    {media: '(prefers-color-scheme: dark)', color: 'black'}
+  ]
 }
 
 /**
  * Root layout component.
+ *
+ * @see https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#root-layout-required
  */
 export default function RootLayout({children}: {children: any}) {
   return (
     <html lang="en">
       <head>
+        <PreloadResources />
         <ColorSchemeScript />
-        <Meta />
       </head>
       <body>
         <MantineProvider theme={theme} defaultColorScheme="auto">
