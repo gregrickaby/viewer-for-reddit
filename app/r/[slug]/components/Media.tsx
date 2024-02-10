03b48be52a1b4ea77435a3891a096ca483bf8f73
Greg Rickaby <gregrickaby@gmail.com>
2024-02-10 09:32:53 -0600

    refactor media component. add lots of comments

--- app/r/[slug]/components/Media.tsx
+++ app/r/[slug]/components/Media.tsx
@@ -2,60 +2,70 @@ import HlsPlayer from '@/app/r/[slug]/components/HlsPlayer'
 import {RedditPost, ImageAsset} from '@/lib/types'
 
 /**
- * Pluck out a medium sized image.
- */
-function getMediumImage(images: ImageAsset[]): ImageAsset | null {
-  // If there are no images, return null.
-  if (!Array.isArray(images) || images.length === 0) {
-    return null
-  }
-
-  // Try to find an image with 640px resolution.
-  const mediumSize = images.find((res) => res.width === 640)
-
-  // Return the medium sized image; otherwise, return the highest resolution.
-  return mediumSize || images[images.length - 1]
-}
-
-/**
  * The media component.
  */
 export default function Media(post: RedditPost) {
   /**
-   * Reddit images can be huge. Try to find a medium sized image.
+   * Helper function to get the medium sized image.
    */
+  const getMediumImage = (images: ImageAsset[]): ImageAsset | null => {
+    if (!Array.isArray(images) || images.length === 0) {
+      return null
+    }
+    const mediumSize = images.find((res) => res.width === 640)
+    return mediumSize || images[images.length - 1]
+  }
+
+  // Set the medium image asset.
   const mediumImageAsset = getMediumImage(post.preview.images[0].resolutions)
 
+  // Set HLS player defaults.
+  const hlsDefaults = {
+    autoPlay: false,
+    controls: true,
+    crossOrigin: 'anonymous',
+    loop: true,
+    muted: true,
+    playsInline: true,
+    preload: 'metadata'
+  }
+
   /**
-   * Read the post hint, then render the appropriate media.
+   * Determine the media type and render the appropriate component.
    */
   switch (post.post_hint) {
+    /**
+     * Image.
+     */
     case 'image':
       return (
-        <a href={post.permalink}>
+        <a
+          aria-label="view full image"
+          href={`https://www.reddit.com${post.permalink}`}
+          rel="noopener noreferrer"
+        >
           <img
-            alt={post.title}
+            alt={post.title || 'reddit image'}
             data-hint="image"
             decoding="async"
-            loading="lazy"
             height={mediumImageAsset?.height}
+            loading="lazy"
             src={mediumImageAsset?.url}
             width={mediumImageAsset?.width}
           />
         </a>
       )
+
+    /**
+     * Hosted video.
+     */
     case 'hosted:video':
       return (
         <HlsPlayer
-          autoPlay={false}
-          controls
-          crossOrigin="anonymous"
+          {...hlsDefaults}
           dataHint="hosted:video"
           height={post.media?.reddit_video?.height}
-          loop
-          playsInline
           poster={mediumImageAsset?.url}
-          preload="metadata"
           src={post.video_preview?.hls_url}
           width={post.media?.reddit_video?.width}
         >
@@ -65,19 +75,20 @@ export default function Media(post: RedditPost) {
           />
         </HlsPlayer>
       )
+
+    /**
+     * Rich video.
+     *
+     * This can either be a video or an iframe.
+     */
     case 'rich:video':
+      // Iframes are evil. Try to use video preview first.
       return post.preview.reddit_video_preview ? (
         <HlsPlayer
-          autoPlay={false}
-          controls
-          crossOrigin="anonymous"
+          {...hlsDefaults}
           dataHint="rich:video"
           height={post.preview.reddit_video_preview.height}
-          loop
-          muted
-          playsInline
           poster={mediumImageAsset?.url}
-          preload="metadata"
           src={post.preview.reddit_video_preview.hls_url}
           width={post.preview.reddit_video_preview.width}
         >
@@ -87,6 +98,7 @@ export default function Media(post: RedditPost) {
           />
         </HlsPlayer>
       ) : (
+        // Otherwise, fallback to the iframe, but sandbox the heck out of it.
         <div className="w-64">
           <iframe
             allow="fullscreen"
@@ -104,43 +116,41 @@ export default function Media(post: RedditPost) {
           />
         </div>
       )
+
+    /**
+     * Links to Giphy or other gif hosting services.
+     *
+     * Convert these to MP4s for the HLS player.
+     */
     case 'link':
-      // Search for .gifv and use the mp4 version.
+      // If the URL is a gifv...
       return post.url.includes('gifv') ? (
         <HlsPlayer
-          autoPlay={false}
-          controls
-          crossOrigin="anonymous"
+          {...hlsDefaults}
           dataHint="link:gifv"
-          loop
-          muted
-          playsInline
           poster={mediumImageAsset?.url}
           src={post.video_preview?.hls_url}
-          preload="metadata"
         >
           <source src={post.url.replace('.gifv', '.mp4')} type="video/mp4" />
         </HlsPlayer>
       ) : (
         // Otherwise, just play the video.
         <HlsPlayer
-          autoPlay={false}
-          controls
-          crossOrigin="anonymous"
+          {...hlsDefaults}
           dataHint="link"
           height={post.video_preview?.height}
-          loop
-          muted
-          playsInline
           poster={mediumImageAsset?.url}
-          preload="metadata"
           src={post.video_preview?.hls_url}
           width={post.video_preview?.width}
         >
           <source src={post.video_preview?.fallback_url} type="video/mp4" />
         </HlsPlayer>
       )
+
+    /**
+     * Nothing matched.
+     */
     default:
-      return <></>
+      return <p>Unsupported or missing media content.</p>
   }
 }
