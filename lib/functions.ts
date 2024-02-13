c78d7ee2803d4b377b5244d509b29619625eaa7e
Greg Rickaby <gregrickaby@gmail.com>
2024-02-13 08:24:38 -0600

    add time helper

--- lib/functions.ts
+++ lib/functions.ts
@@ -10,3 +10,29 @@ export function getMediumImage(images: ImageAsset[]): ImageAsset | null {
   const mediumSize = images.find((res) => res.width === 640)
   return mediumSize || images[images.length - 1]
 }
+
+/**
+ * Helper function to get the time in "___ ago" format.
+ */
+export function getTimeAgo(timestampInSeconds: number): string {
+  const secondsElapsed = Date.now() / 1000 - timestampInSeconds
+  const minutesElapsed = Math.floor(secondsElapsed / 60)
+  const hoursElapsed = Math.floor(secondsElapsed / 3600)
+  const daysElapsed = Math.floor(secondsElapsed / 86400)
+  const monthsElapsed = Math.floor(secondsElapsed / 2592000)
+  const yearsElapsed = Math.floor(secondsElapsed / 31536000)
+
+  if (minutesElapsed < 1) {
+    return 'just now'
+  } else if (minutesElapsed < 60) {
+    return `${minutesElapsed}m ago`
+  } else if (hoursElapsed < 24) {
+    return `${hoursElapsed}h ago`
+  } else if (daysElapsed < 30) {
+    return `${daysElapsed}d ago`
+  } else if (monthsElapsed < 12) {
+    return `${monthsElapsed}mo ago`
+  } else {
+    return `${yearsElapsed}y ago`
+  }
+}
