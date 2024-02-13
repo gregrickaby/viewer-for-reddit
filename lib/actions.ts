8e541e3418bf38400bafca572b1bcf3f5c0c080f
Greg Rickaby <gregrickaby@gmail.com>
2024-02-13 14:37:55 -0600

    define scope and sanitize/validate

--- lib/actions.ts
+++ lib/actions.ts
@@ -17,7 +17,7 @@ export async function fetchToken(): Promise<RedditTokenResponse> {
   try {
     // Fetch the Reddit oAuth token.
     const response = await fetch(
-      'https://www.reddit.com/api/v1/access_token?grant_type=client_credentials&device_id=DO_NOT_TRACK_THIS_DEVICE',
+      'https://www.reddit.com/api/v1/access_token?grant_type=client_credentials&scope=read&device_id=DO_NOT_TRACK_THIS_DEVICE',
       {
         method: 'POST',
         headers: {
@@ -67,6 +67,9 @@ export async function fetchSearchResults(
     // Get the access token.
     const {access_token} = await fetchToken()
 
+    // Validate and sanitize the query.
+    query = query && query.replace(/[^a-zA-Z0-9_]/g, '')
+
     // Attempt to fetch subreddits.
     const response = await fetch(
       `https://oauth.reddit.com/api/subreddit_autocomplete_v2?query=${query}&limit=10&include_over_18=true&include_profiles=false&typeahead_active=true&search_query_id=DO_NOT_TRACK`,
@@ -107,11 +110,31 @@ export async function fetchSubredditPosts(
   props: FetchSubredditProps
 ): Promise<RedditPostResponse> {
   try {
-    // Destructure props.
-    const {slug, sort, limit, after} = props
-
     // Fetch the Reddit oAuth token.
     const {access_token} = await fetchToken()
+
+    // Destructure props.
+    let {slug, sort, limit, after} = props
+
+    // Validate and sanitize the slug param.
+    slug = slug && slug.replace(/[^a-zA-Z0-9_]/g, '')
+
+    // Validate the sort param.
+    if (!['hot', 'new', 'top', 'rising'].includes(sort)) {
+      throw new Error(
+        'Invalid sort parameter. Allowed values: hot, new, top, rising.'
+      )
+    }
+
+    // Validate the limit param.
+    if ((limit && limit < 1) || limit > 100) {
+      throw new Error('Invalid limit parameter. Allowed values: 1-100.')
+    }
+
+    // Validate the after param.
+    if (after && !after.match(/^[a-zA-Z0-9_]+$/)) {
+      throw new Error('Invalid after parameter. Must be alphanumeric.')
+    }
 
     // Fetch the subreddit posts.
     const response = await fetch(
