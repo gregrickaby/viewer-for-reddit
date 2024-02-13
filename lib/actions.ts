de76283496d224b9cf9134c427b3dc34d261380a
Greg Rickaby <gregrickaby@gmail.com>
2024-02-13 07:47:02 -0600

    move actions to /lib

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,12 +1,7 @@
 'use server'
 
 import config from '@/lib/config'
-import {
-  FetchSubredditProps,
-  RedditPostResponse,
-  RedditSearchResponse,
-  RedditTokenResponse
-} from '@/lib/types'
+import {RedditSearchResponse, RedditTokenResponse} from '@/lib/types'
 
 /**
  * Fetch a Reddit oAuth token.
@@ -14,6 +9,7 @@ import {
  * @see https://github.com/reddit-archive/reddit/wiki/OAuth2#application-only-oauth
  */
 export async function fetchToken(): Promise<RedditTokenResponse> {
+  'use server'
   try {
     // Fetch the Reddit oAuth token.
     const response = await fetch(
@@ -96,67 +92,6 @@ export async function fetchSearchResults(
     return data
   } catch (error) {
     console.error(`Exception thrown in fetchSearchResults(): ${error}`)
-    return {error: `${error}`}
-  }
-}
-
-/**
- * Fetch subreddit posts.
- */
-export async function fetchSubredditPosts(
-  props: FetchSubredditProps
-): Promise<RedditPostResponse> {
-  try {
-    // Destructure props.
-    const {slug, sortBy, limit, after} = props
-
-    // Fetch the Reddit oAuth token.
-    const {access_token} = await fetchToken()
-
-    // Fetch the subreddit posts.
-    const response = await fetch(
-      `https://oauth.reddit.com/r/${slug}/${sortBy}/.json?limit=${limit}&after=${after}&raw_json=1`,
-      {
-        headers: {
-          'User-Agent': config.userAgent,
-          authorization: `Bearer ${access_token}`
-        },
-        next: {
-          tags: [slug],
-          revalidate: config.cacheTtl
-        }
-      }
-    )
-
-    // Bad response? Bail.
-    if (!response.ok) {
-      throw new Error(` ${response.statusText}: /r/${slug}`)
-    }
-
-    // Parse the response.
-    const data = (await response.json()) as RedditPostResponse
-
-    // If the response is empty, bail.
-    if (!data.data) {
-      throw new Error('Failed to parse subreddit response.')
-    }
-
-    // Return the posts.
-    return {
-      kind: data.kind,
-      data: {
-        modhash: data.data.modhash,
-        dist: data.data.dist,
-        children: data.data.children.filter(
-          ({data}) =>
-            data.post_hint && data.post_hint !== 'self' && !data.poststickied // Exclude self/stickied posts.
-        ),
-        after: data.data.after,
-        before: data.data.before
-      }
-    }
-  } catch (error) {
-    console.error(`Exception thrown in fetchSubredditPosts(): ${error}`)
     return {error: `${error}`}
   }
 }
