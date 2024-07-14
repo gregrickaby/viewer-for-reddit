2043995f97b20d1e03fc1170cd0760b7b0bacce9
Greg Rickaby <gregrickaby@gmail.com>
2024-07-14 12:57:31 -0500

    bump to 2 instances

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'reddit-viewer.com', // Name of the app.
       script: 'npm', // Specifies the script runner.
       args: 'start', // Specifies the script to run.
-      instances: 1, // Can be scaled up if needed.
+      instances: 2, // Can be scaled up if needed.
       autorestart: true, // Automatically restart if the app crashes.
       watch: false, // Turned off in production to avoid unintentional restarts.
       max_memory_restart: '1G', // Restart the app if it reaches the memory limit.
