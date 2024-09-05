589a2a3ea19110d3fe672c89293bab680c691cb7
Greg Rickaby <greg.rickaby@mindsize.com>
2024-09-05 10:22:33 -0500

    update actions

--- lib/actions.ts
+++ lib/actions.ts
@@ -10,30 +10,52 @@ import {
 } from '@/lib/types'
 
 /**
+ * Validate environment variables.
+ */
+function validateEnvVars() {
+  const clientId = process.env.REDDIT_CLIENT_ID
+  const clientSecret = process.env.REDDIT_CLIENT_SECRET
+
+  if (!clientId || !clientSecret) {
+    throw new Error('Missing Reddit environment variables!')
+  }
+
+  return {clientId, clientSecret}
+}
+
+/**
  * Fetch a Reddit oAuth token.
  *
  * @see https://github.com/reddit-archive/reddit/wiki/OAuth2#application-only-oauth
  */
 export async function fetchToken(): Promise<RedditTokenResponse> {
   try {
+    // Get the client ID and secret.
+    const {clientId, clientSecret} = validateEnvVars()
+
+    // Build the URL.
+    const url = new URL('https://www.reddit.com/api/v1/access_token')
+
+    // Build the query parameters.
+    url.searchParams.append('grant_type', 'client_credentials')
+    url.searchParams.append('scope', 'read')
+    url.searchParams.append('device_id', 'DO_NOT_TRACK_THIS_DEVICE')
+
     // Fetch the Reddit oAuth token.
-    const response = await fetch(
-      'https://www.reddit.com/api/v1/access_token?grant_type=client_credentials&scope=read&device_id=DO_NOT_TRACK_THIS_DEVICE',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'User-Agent': config.userAgent,
-          Authorization: `Basic ${btoa(
-            `${process.env.REDDIT_CLIENT_ID}:${process.env.REDDIT_CLIENT_SECRET}`
-          )}`
-        },
-        next: {
-          tags: ['token'],
-          revalidate: config.cacheTtl
-        }
+    const response = await fetch(url.toString(), {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'User-Agent': config.userAgent,
+        Authorization: `Basic ${Buffer.from(
+          `${clientId}:${clientSecret}`
+        ).toString('base64')}`
+      },
+      next: {
+        tags: ['token'],
+        revalidate: config.cacheTtl
       }
-    )
+    })
 
     // Bad response? Bail.
     if (!response.ok) {
@@ -41,17 +63,14 @@ export async function fetchToken(): Promise<RedditTokenResponse> {
     }
 
     // Parse the response.
-    const data = (await response.json()) as RedditTokenResponse
+    const data: RedditTokenResponse = await response.json()
 
     // If the response is empty, bail.
     if (!data.access_token) {
-      throw new Error(data.error)
+      throw new Error(data.error || 'No access token in response')
     }
 
-    // Return the token.
-    return {
-      access_token: data.access_token
-    }
+    return data
   } catch (error) {
     console.error(`Exception thrown in fetchToken(): ${error}`)
     return {error: `${error}`}
@@ -60,12 +79,14 @@ export async function fetchToken(): Promise<RedditTokenResponse> {
 
 /**
  * Fetch search results.
+ *
+ * @see https://www.reddit.com/dev/api/oauth#GET_api_subreddit_autocomplete_v2
  */
 export async function fetchSearchResults(
   query: string
 ): Promise<RedditSearchResponse> {
   try {
-    // Get the access token.
+    // Get the access token result.
     const {access_token} = await fetchToken()
 
     // No token? Bail.
@@ -73,30 +94,40 @@ export async function fetchSearchResults(
       throw new Error('Failed to fetch Reddit oAuth Token.')
     }
 
-    // Validate and sanitize the query.
-    query = query?.replace(/\W/g, '')
+    // Build the search URL.
+    const url = new URL(
+      'https://oauth.reddit.com/api/subreddit_autocomplete_v2'
+    )
+
+    // Append query parameters.
+    url.searchParams.append('query', encodeURIComponent(query.trim()))
+    url.searchParams.append('limit', '10')
+    url.searchParams.append('include_over_18', 'true')
+    url.searchParams.append('include_profiles', 'false')
+    url.searchParams.append('typeahead_active', 'true')
+    url.searchParams.append('search_query_id', 'DO_NOT_TRACK')
 
     // Attempt to fetch subreddits.
-    const response = await fetch(
-      `https://oauth.reddit.com/api/subreddit_autocomplete_v2?query=${query}&limit=10&include_over_18=true&include_profiles=false&typeahead_active=true&search_query_id=DO_NOT_TRACK`,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'User-Agent': config.userAgent,
-          Authorization: `Bearer ${access_token}`
-        },
-        next: {
-          revalidate: config.cacheTtl
-        }
+    const response = await fetch(url.toString(), {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'User-Agent': config.userAgent,
+        Authorization: `Bearer ${access_token}`
+      },
+      next: {
+        tags: [`search-${query}`],
+        revalidate: config.cacheTtl
       }
-    )
+    })
+
     // Bad response? Bail.
     if (!response.ok) {
       throw new Error(`Failed to fetch search results. ${response.statusText}`)
     }
 
     // Parse the response.
-    const data = (await response.json()) as RedditSearchResponse
+    const data: RedditSearchResponse = await response.json()
 
     // If the response is empty, bail.
     if (!data.data) {
@@ -113,8 +144,6 @@ export async function fetchSearchResults(
 
 /**
  * Fetch subreddit posts.
- *
- * @see
  */
 export async function fetchSubredditPosts(
   props: FetchSubredditProps
@@ -128,57 +157,78 @@ export async function fetchSubredditPosts(
       throw new Error('Failed to fetch Reddit oAuth Token.')
     }
 
-    // Destructure props.
-    let {slug, sort, limit, after} = props
+    // Destructure and validate props.
+    let {slug, sort, limit = 10, after = ''} = props
 
-    // Validate and sanitize the slug param.
-    slug = slug?.replace(/\W/g, '')
-
-    // Validate the sort param.
-    if (!['hot', 'new', 'top', 'rising'].includes(sort)) {
+    // Validate sort.
+    const validSorts = ['hot', 'new', 'top', 'rising']
+    if (!validSorts.includes(sort)) {
       throw new Error(
-        'Invalid sort parameter. Allowed values: hot, new, top, rising.'
+        `Invalid sort parameter. Allowed values: ${validSorts.join(', ')}.`
       )
     }
 
-    // Validate the limit param.
-    if ((limit && limit < 1) || limit > 100) {
-      throw new Error('Invalid limit parameter. Allowed values: 1-100.')
+    // Validate limit.
+    if (limit < 1 || limit > 100) {
+      throw new Error(
+        `Invalid limit parameter. Allowed values: 1-100. ${limit}`
+      )
     }
 
-    // Validate the after param.
-    if (RegExp(/w/).exec(after)) {
-      throw new Error('Invalid after parameter. Must be alphanumeric.')
+    // Validate after param (ensure it's alphanumeric or empty).
+    if (after && !/^[a-zA-Z0-9_]+$/.test(after)) {
+      throw new Error(`Invalid after parameter. Must be alphanumeric. ${after}`)
     }
 
-    // Fetch the subreddit posts.
-    const response = await fetch(
-      `https://oauth.reddit.com/r/${slug}/${sort}/.json?limit=${limit}&after=${after}&raw_json=1`,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'User-Agent': config.userAgent,
-          Authorization: access_token
-        },
-        next: {
-          tags: [`posts-${slug}-${sort}-${limit}-${after}`],
-          revalidate: config.cacheTtl
-        }
+    // Build the URL.
+    const url = new URL(`https://oauth.reddit.com/r/${slug}/${sort}/.json`)
+
+    // Append query parameters.
+    const searchParams = new URLSearchParams({
+      limit: limit.toString(),
+      after,
+      raw_json: '1'
+    })
+    url.search = searchParams.toString()
+
+    // Fetch subreddit posts.
+    const response = await fetch(url.toString(), {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'User-Agent': config.userAgent,
+        Authorization: `Bearer ${access_token}`
+      },
+      next: {
+        tags: [`posts-${slug}-${sort}-${limit}-${after}`],
+        revalidate: config.cacheTtl
       }
-    )
+    })
+
+    // Extract and log rate limit headers.
+    const rateLimitUsed = response.headers.get('X-Ratelimit-Used')
+    const rateLimitRemaining = response.headers.get('X-Ratelimit-Remaining')
+    const rateLimitReset = response.headers.get('X-Ratelimit-Reset')
 
     // Bad response? Bail.
     if (!response.ok) {
-      throw new Error(` ${response.statusText}: /r/${slug}`)
+      throw new Error(
+        `Failed to fetch subreddit posts. ${response.statusText} | Rate Limit Used: ${rateLimitUsed}, Remaining: ${rateLimitRemaining}, Resets in: ${rateLimitReset} seconds`
+      )
     }
-
     // Parse the response.
-    const data = (await response.json()) as RedditPostResponse
+    const data: RedditPostResponse = await response.json()
 
     // If the response is empty, bail.
     if (!data.data) {
       throw new Error('Failed to parse subreddit response.')
     }
+
+    // Filter out unwanted posts (exclude self/stickied posts).
+    const filteredChildren = data.data.children.filter(
+      ({data}) =>
+        data.post_hint && data.post_hint !== 'self' && !data.poststickied
+    )
 
     // Return the posts.
     return {
@@ -186,10 +236,7 @@ export async function fetchSubredditPosts(
       data: {
         modhash: data.data.modhash,
         dist: data.data.dist,
-        children: data.data.children.filter(
-          ({data}) =>
-            data.post_hint && data.post_hint !== 'self' && !data.poststickied // Exclude self/stickied posts.
-        ),
+        children: filteredChildren,
         after: data.data.after,
         before: data.data.before
       }
@@ -218,23 +265,24 @@ export async function fetchSubredditAbout(
     }
 
     // Validate and sanitize the slug prop.
-    slug = slug?.replace(/\W/g, '')
+    slug = encodeURIComponent(slug.trim())
 
-    // Fetch the subreddit about.
-    const response = await fetch(
-      `https://oauth.reddit.com/r/${slug}/about/.json`,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'User-Agent': config.userAgent,
-          Authorization: access_token
-        },
-        next: {
-          tags: [`about-${slug}`],
-          revalidate: config.cacheTtl
-        }
+    // Build the URL.
+    const url = new URL(`https://oauth.reddit.com/r/${slug}/about/.json`)
+
+    // Fetch the subreddit about information.
+    const response = await fetch(url.toString(), {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'User-Agent': config.userAgent,
+        Authorization: `Bearer ${access_token}`
+      },
+      next: {
+        tags: [`about-${slug}`],
+        revalidate: 86400 // 24 hours.
       }
-    )
+    })
 
     // Bad response? Bail.
     if (!response.ok) {
@@ -242,14 +290,14 @@ export async function fetchSubredditAbout(
     }
 
     // Parse the response.
-    const data = (await response.json()) as RedditAboutResponse
+    const data: RedditAboutResponse = await response.json()
 
     // If the response is empty, bail.
     if (!data.data) {
       throw new Error('Failed to parse subreddit about response.')
     }
 
-    // Return the about.
+    // Return the about information.
     return data
   } catch (error) {
     console.error(`Exception thrown in fetchSubredditAbout(): ${error}`)
