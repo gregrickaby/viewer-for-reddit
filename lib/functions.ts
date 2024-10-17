77df57c1d2adb64afa3989615221efc2d2b4bf74
Greg Rickaby <greg.rickaby@mindsize.com>
2024-10-17 15:10:50 -0500

    add debounce

--- lib/functions.ts
+++ lib/functions.ts
@@ -66,3 +66,23 @@ export function logError(error: unknown): void {
     console.error(`Error: ${String(error)}`)
   }
 }
+
+/**
+ * Creates a debounced version of a function.
+ *
+ * @param func The function to debounce.
+ * @param delay The number of milliseconds to delay.
+ */
+export function debounce<T extends (...args: Parameters<T>) => ReturnType<T>>(
+  func: T,
+  delay: number
+) {
+  let timer: ReturnType<typeof setTimeout>
+
+  return function (...args: Parameters<T>): void {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      func(...args)
+    }, delay)
+  }
+}
