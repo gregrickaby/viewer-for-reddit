5f1a5b380d9b4434d85a6f0ce31328f2fa86b015
Greg Rickaby <greg.rickaby@mindsize.com>
2024-10-25 14:05:25 -0500

    update redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async redirects() {
-    if (process.env.VERCEL_ENV === 'production') {
-      return [
-        {
-          source: '/',
-          destination: 'https://reddit-viewer.com/',
-          permanent: true
-        },
-        {
-          source: '/r/:path*',
-          destination: 'https://reddit-viewer.com/r/:path*',
-          permanent: true
-        }
-      ]
-    } else {
-      return [
-        {
-          source: '/r',
-          destination: '/',
-          permanent: true
-        }
-      ]
-    }
+    return [
+      {
+        source: '/r',
+        destination: '/',
+        permanent: true
+      }
+    ]
   },
   logging: {
     fetches: {
