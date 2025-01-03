bac2147365b31c41c92e92bcc10fcf3c789043d4
Greg Rickaby <gregrickaby@gmail.com>
2025-01-03 07:18:32 -0600

    remove redirects

--- next.config.ts
+++ next.config.ts
@@ -1,41 +1,6 @@
 import type {NextConfig} from 'next'
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    // If the app is deployed on Vercel, redirect the old URLs to the new domain.
-    if (process.env.VERCEL_ENV === 'production') {
-      return [
-        {
-          source: 'https://viewer-for-reddit.vercel.app',
-          destination: 'https://reddit-viewer.com/',
-          permanent: true
-        },
-        {
-          source: 'https://reddit-image-viewer.vercel.app',
-          destination: 'https://reddit-viewer.com/',
-          permanent: true
-        },
-        {
-          source: 'https://redditviewer.vercel.app',
-          destination: 'https://reddit-viewer.com/',
-          permanent: true
-        },
-        {
-          source: '/r/:path*',
-          destination: 'https://reddit-viewer.com/r/:path*',
-          permanent: true
-        }
-      ]
-    } else {
-      return [
-        {
-          source: '/r',
-          destination: '/',
-          permanent: true
-        }
-      ]
-    }
-  },
   logging: {
     fetches: {
       fullUrl: true
