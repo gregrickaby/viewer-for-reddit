fe58d4c2f3ddc35498a665a547184bab4062ac61
Greg Rickaby <gregrickaby@gmail.com>
2025-02-21 10:59:58 -0600

    add alias support. adjust for new paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 import vercel from 'vite-plugin-vercel'
 import { defineConfig } from 'vitest/config'
 
-// https://vite.dev/config/
+/**
+ * Vite configuration.
+ *
+ * @see https://vitejs.dev/config/
+ */
 export default defineConfig({
   plugins: [react(), tailwindcss(), vercel()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './')
+    }
+  },
   base: '/',
   server: {
     port: 3000,
@@ -17,7 +27,7 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     mockReset: true,
-    setupFiles: ['./setupTests.ts'],
+    setupFiles: ['./tests/utils/setupTests.ts'],
     coverage: {
       enabled: true,
       exclude: ['**/__tests__/**']
