da9a5265e279fbf9085088aee5ca0d97db6feb0b
Greg Rickaby <gregrickaby@gmail.com>
2025-02-21 12:01:54 -0600

    fix intersectionobeserver mock

--- tests/utils/setupTests.ts
+++ tests/utils/setupTests.ts
@@ -41,22 +41,21 @@ Object.defineProperty(window, 'matchMedia', {
 })
 
 /**
- * Mock ResizeObserver.
- */
-global.ResizeObserver = vi.fn().mockImplementation(() => ({
-  observe: vi.fn(),
-  unobserve: vi.fn(),
-  disconnect: vi.fn()
-}))
-
-/**
  * Mock IntersectionObserver.
  */
-global.IntersectionObserver = vi.fn().mockImplementation(() => ({
-  observe: vi.fn(),
-  unobserve: vi.fn(),
-  disconnect: vi.fn(),
-  root: null,
-  rootMargin: '',
-  thresholds: []
-}))
+global.IntersectionObserver = class IntersectionObserver
+  implements IntersectionObserver
+{
+  readonly root: Element | Document | null = null
+  readonly rootMargin: string = ''
+  readonly thresholds: ReadonlyArray<number> = []
+  observe: (target: Element) => void
+  unobserve: (target: Element) => void
+  disconnect: () => void
+
+  constructor() {
+    this.observe = vi.fn()
+    this.unobserve = vi.fn()
+    this.disconnect = vi.fn()
+  }
+} as unknown as typeof IntersectionObserver
