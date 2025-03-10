57a8c156e572c479e5d9150d3df814b0477a37e7
Greg Rickaby <gregrickaby@gmail.com>
2025-03-10 14:51:01 -0500

    add test to ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,13 +2,27 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    coverage: {
+      enabled: true,
+      exclude: [
+        '**/*.config.{ts,tsx}',
+        '**/*.next/**',
+        '**/*.spec.{ts,tsx}',
+        '**/*.stories.{ts,tsx}',
+        '**/tests/**',
+        '**/node_modules/**',
+        '**/*.types.ts',
+        '**/*.d.ts'
+      ]
+    },
     environment: 'jsdom',
     globals: true,
     mockReset: true,
     setupFiles: ['./tests/utils/setupTests.ts'],
-    coverage: {
-      enabled: true,
-      exclude: ['**/tests/**']
-    }
+    exclude: [
+      '**/node_modules/**',
+      '**/*.stories.{ts,tsx}',
+      '**/*.spec.{ts,tsx}'
+    ]
   }
 })
