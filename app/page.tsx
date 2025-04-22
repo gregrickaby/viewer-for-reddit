0bc8eb28c5e204f410e47ff30c5011fa298f0d16
Greg Rickaby <gregrickaby@gmail.com>
2025-04-21 19:28:33 -0500

    add trending

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,31 @@
+import {fetchPopularSubreddits} from '@/lib/actions'
+
 /**
  * The home page route.
  *
  * @see https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#pages
  */
-export default function Home() {
+export default async function Home() {
+  const popular = await fetchPopularSubreddits()
   return (
     <section>
+      <h2 className="">Trending</h2>
+      {popular.data?.children.length ? (
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+          {popular.data.children.map((subreddit) => (
+            <a
+              className="button"
+              href={`/r/${subreddit.data.display_name}`}
+              key={subreddit.data.id}
+            >
+              {subreddit.data.display_name}
+            </a>
+          ))}
+        </div>
+      ) : (
+        <p>No popular subreddits found.</p>
+      )}
+      <h2>About</h2>
       <p>
         The Viewer for Reddit app is the best way to browse media on Reddit
         anonymously.
@@ -14,7 +34,8 @@ export default function Home() {
         With a focus on privacy, there&apos;s no tracking or logging of your
         activity—aside from the minimal server logs (maintained for operational
         integrity). Enjoy a clean, fast, and untracked browsing experience. To
-        get started, try searching for a subreddit above.
+        get started, try searching for a subreddit or click one of the popular
+        subreddits above.
       </p>
     </section>
   )
