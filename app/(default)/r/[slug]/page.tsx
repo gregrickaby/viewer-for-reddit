5756041c3962e0d669523b2de5d2e4d0d0947c28
Greg Rickaby <gregrickaby@gmail.com>
2025-05-10 14:38:31 -0500

    initial commit of results page

--- app/(default)/r/[slug]/page.tsx
+++ app/(default)/r/[slug]/page.tsx
@@ -1,3 +1,30 @@
-export default async function Page() {
-  return <>Subreddit page</>
+import {Results} from '@/components/Results/Results'
+import config from '@/lib/config'
+import type {Params, SearchParams, SortingOption} from '@/lib/types'
+
+/**
+ * Generate metadata.
+ */
+export async function generateMetadata(props: {params: Params}) {
+  const params = await props.params
+  const slug = params.slug
+
+  return {
+    title: `/r/${slug} - ${config.siteName}`
+  }
+}
+
+/**
+ * The single subreddit page.
+ */
+export default async function Page(props: {
+  params: Params
+  searchParams: SearchParams
+}) {
+  const params = await props.params
+  const slug = params.slug
+  const searchParams = await props.searchParams
+  const sort = searchParams.sort as SortingOption
+
+  return <Results subreddit={slug} sort={sort} />
 }
