997c7ac970039fb6f06400fff0d1e8f43ab187e2
Greg Rickaby <gregrickaby@gmail.com>
2025-05-10 16:16:37 -0500

    initial commit of nav with fav and recent

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,5 +1,43 @@
 'use client'
 
+import {useAppSelector} from '@/lib/store/hooks'
+import {useMounted} from '@mantine/hooks'
+import Link from 'next/link'
+
 export function Navigation() {
-  return <></>
+  const mounted = useMounted()
+  const recent = useAppSelector((state) => state.settings.recent)
+  const favorites = useAppSelector((state) => state.settings.favorites)
+
+  if (!mounted) return null
+
+  return (
+    <nav>
+      {favorites.length > 0 && (
+        <div>
+          <h2>Favorites</h2>
+          <ul>
+            {favorites.map((sub) => (
+              <li key={sub.display_name}>
+                <Link href={`/r/${sub.display_name}`}>{sub.display_name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      {recent.length > 0 && (
+        <div>
+          <h2>Recent</h2>
+          <ul>
+            {recent.map((sub) => (
+              <li key={sub.display_name}>
+                <Link href={`/r/${sub.display_name}`}>{sub.display_name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </nav>
+  )
 }
