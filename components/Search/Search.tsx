dd3a1157e37ca4210ab5548595050b29df9b94ed
Greg Rickaby <gregrickaby@gmail.com>
2025-05-10 14:38:08 -0500

    updated search w/ link

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,34 +1,53 @@
 'use client'
 
 import {useSearchSubredditsQuery} from '@/lib/store/services/privateApi'
+import {useGetPopularSubredditsQuery} from '@/lib/store/services/publicApi'
 import {Autocomplete} from '@mantine/core'
 import {useDebouncedValue} from '@mantine/hooks'
-import {useState} from 'react'
+import Link from 'next/link'
+import {useMemo, useState} from 'react'
 import classes from './Search.module.css'
 
 export function Search() {
   const [query, setQuery] = useState('')
   const [debounced] = useDebouncedValue(query.trim(), 200)
 
-  const {data: subreddits = []} = useSearchSubredditsQuery(
+  const {data: searchResults = []} = useSearchSubredditsQuery(
     {query: debounced, enableNsfw: false},
     {skip: !debounced}
   )
 
-  const autoCompleteData = subreddits.map((s) => s.display_name_prefixed ?? '')
+  const {data: popularResponse} = useGetPopularSubredditsQuery(
+    {limit: 25},
+    {skip: !!debounced}
+  )
+
+  const autoCompleteData = useMemo(() => {
+    if (debounced) {
+      return searchResults.map((s) => s.display_name_prefixed ?? '')
+    }
+
+    return (
+      popularResponse?.data?.children.map(
+        (child) => child.data.display_name_prefixed ?? ''
+      ) ?? []
+    )
+  }, [debounced, searchResults, popularResponse])
 
   return (
     <Autocomplete
-      aria-label="search subreddits"
-      classNames={{
-        wrapper: classes.wrapper
-      }}
+      aria-label="Search subreddits"
+      classNames={{wrapper: classes.wrapper}}
       clearable
       data={autoCompleteData}
       onChange={setQuery}
       placeholder="Search subreddits"
       value={query}
-      width="100%"
+      renderOption={({option}) => (
+        <Link href={`/r/${option.value.replace(/^r\//, '')}`}>
+          {option.value}
+        </Link>
+      )}
     />
   )
 }
