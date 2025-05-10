afbbe6060d3a20d828fee6f07071f5175f19f474
Greg Rickaby <gregrickaby@gmail.com>
2025-05-10 12:10:21 -0500

    add polyfill

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,7 +1,12 @@
 import {setupBrowserMocks} from '@/test-utils/mocks/browserMocks'
 import '@testing-library/jest-dom'
+import {URLSearchParams as NodeURLSearchParams} from 'url'
 import {afterAll, afterEach, beforeAll} from 'vitest'
 import {server} from './test-utils/msw/server'
+
+// Polyfill for URLSearchParams in Node.js.
+// https://github.com/vitest-dev/vitest/issues/7906
+global.URLSearchParams = NodeURLSearchParams as any
 
 setupBrowserMocks()
 
