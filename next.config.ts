0c095d93d70e9c70cf00cecad60b8d0bfc3d99df
Greg Rickaby <gregrickaby@gmail.com>
2025-05-10 21:08:45 -0500

    add support for external images. add other packages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,22 @@
 import type {NextConfig} from 'next'
 
 const nextConfig: NextConfig = {
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.redditmedia.**',
+        pathname: '/**'
+      }
+    ]
+  },
   experimental: {
-    optimizePackageImports: ['@mantine/core', '@mantine/hooks']
+    optimizePackageImports: [
+      '@mantine/core',
+      '@mantine/hooks',
+      '@mantine/notifications'
+    ]
   },
   logging: {
     fetches: {
