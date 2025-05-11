4d5d6cf585835b9987fb67784601c2e4536558d4
Greg Rickaby <gregrickaby@gmail.com>
2025-05-10 19:11:32 -0500

    add settings

---
+++ components/Settings/Settings.tsx
@@ -0,0 +1,116 @@
+'use client'
+
+import {
+  clearFavorites,
+  clearRecent,
+  resetSettings,
+  toggleNsfw
+} from '@/lib/store/features/settingsSlice'
+import {useAppDispatch, useAppSelector} from '@/lib/store/hooks'
+import {
+  ActionIcon,
+  Menu,
+  Switch,
+  Tooltip,
+  useMantineColorScheme
+} from '@mantine/core'
+import {showNotification} from '@mantine/notifications'
+import {FaCog, FaTrashAlt} from 'react-icons/fa'
+import {GoBookmarkSlashFill} from 'react-icons/go'
+import {TbArticleOff} from 'react-icons/tb'
+
+export function Settings() {
+  const dispatch = useAppDispatch()
+  const {colorScheme, setColorScheme} = useMantineColorScheme()
+  const isDark = colorScheme === 'dark'
+  const nsfw = useAppSelector((state) => state.settings.enableNsfw)
+
+  return (
+    <Menu shadow="md" width={220} position="bottom-end" withArrow>
+      <Menu.Target>
+        <ActionIcon size="lg" aria-label="Settings">
+          <FaCog />
+        </ActionIcon>
+      </Menu.Target>
+
+      <Menu.Dropdown>
+        <Menu.Label>Preferences</Menu.Label>
+
+        <Menu.Item closeMenuOnClick={false}>
+          <Switch
+            label="Enable NSFW"
+            checked={nsfw}
+            onChange={() => dispatch(toggleNsfw())}
+          />
+        </Menu.Item>
+
+        <Menu.Item closeMenuOnClick={false}>
+          <Switch
+            label="Dark Mode"
+            checked={isDark}
+            onChange={(e) =>
+              setColorScheme(e.currentTarget.checked ? 'dark' : 'light')
+            }
+          />
+        </Menu.Item>
+
+        <Menu.Divider />
+        <Menu.Label>Viewing History and Favorites</Menu.Label>
+
+        <Menu.Item
+          onClick={() => {
+            dispatch(clearRecent())
+            showNotification({
+              title: 'Success',
+              message: 'All recent viewing history has been removed.',
+              color: 'green'
+            })
+          }}
+        >
+          <TbArticleOff style={{marginRight: '8px'}} />
+          Delete Recently Viewed
+        </Menu.Item>
+
+        <Menu.Item
+          onClick={() => {
+            dispatch(clearFavorites())
+            showNotification({
+              title: 'Success',
+              message:
+                'All favorites have been removed. You can always add them again.',
+              color: 'green'
+            })
+          }}
+        >
+          <GoBookmarkSlashFill style={{marginRight: '8px'}} />
+          Delete All Favorites
+        </Menu.Item>
+
+        <Menu.Divider />
+
+        <Menu.Label>Danger Zone</Menu.Label>
+
+        <Tooltip
+          label="This will delete all settings, recent viewing history, and favorites. This action cannot be undone!"
+          position="bottom"
+        >
+          <Menu.Item
+            color="red"
+            onClick={() => {
+              dispatch(resetSettings())
+              showNotification({
+                title: 'Success',
+                message:
+                  'All settings, history, and favorites have been removed.',
+                color: 'green'
+              })
+            }}
+          >
+            <FaTrashAlt style={{marginRight: '8px'}} />
+            Reset All
+          </Menu.Item>
+        </Tooltip>
+      </Menu.Dropdown>
+    </Menu>
+  )
+}
