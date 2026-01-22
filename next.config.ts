8917e2dbb230f3ad609de3e39807e2ea1c4a487d
Greg Rickaby <gregrickaby@gmail.com>
2026-01-22 09:18:03 -0600

    refactor: update dependencies for @redocly/cli and prettier to latest versions, enhance image quality settings

--- next.config.ts
+++ next.config.ts
@@ -39,6 +39,7 @@ const nextConfig: NextConfig = {
     dangerouslyAllowSVG: false,
     contentDispositionType: 'attachment',
     formats: ['image/webp'],
+    qualities: [75, 85],
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384]
   },
