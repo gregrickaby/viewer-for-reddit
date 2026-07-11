7cc2365e389477e36c9cb139d9f01abd31b8374c
Greg Rickaby <gregrickaby@gmail.com>
2026-07-11 08:58:22 -0500

    disable axiom while in dev mode

--- lib/axiom/client.ts
+++ lib/axiom/client.ts
@@ -1,11 +1,14 @@
 'use client'
 
-import {Logger, ProxyTransport} from '@axiomhq/logging'
+import {ConsoleTransport, Logger, ProxyTransport} from '@axiomhq/logging'
 import {createUseLogger, createWebVitalsComponent} from '@axiomhq/react'
 
-export const logger = new Logger({
-  transports: [new ProxyTransport({url: '/api/axiom', autoFlush: true})]
-})
+export const logger =
+  process.env.NODE_ENV === 'production'
+    ? new Logger({
+        transports: [new ProxyTransport({url: '/api/axiom', autoFlush: true})]
+      })
+    : new Logger({transports: [new ConsoleTransport()]})
 
 export const useLogger = createUseLogger(logger)
 export const WebVitals = createWebVitalsComponent(logger)
--- lib/axiom/server.ts
+++ lib/axiom/server.ts
@@ -1,15 +1,18 @@
-import {AxiomJSTransport, Logger} from '@axiomhq/logging'
+import {AxiomJSTransport, ConsoleTransport, Logger} from '@axiomhq/logging'
 import {createAxiomRouteHandler, nextJsFormatters} from '@axiomhq/nextjs'
 import axiomClient from './axiom'
 
-export const logger = new Logger({
-  transports: [
-    new AxiomJSTransport({
-      axiom: axiomClient,
-      dataset: process.env.AXIOM_DATASET!
-    })
-  ],
-  formatters: nextJsFormatters
-})
+export const logger =
+  process.env.NODE_ENV === 'production'
+    ? new Logger({
+        transports: [
+          new AxiomJSTransport({
+            axiom: axiomClient,
+            dataset: process.env.AXIOM_DATASET!
+          })
+        ],
+        formatters: nextJsFormatters
+      })
+    : new Logger({transports: [new ConsoleTransport()]})
 
 export const withAxiom = createAxiomRouteHandler(logger)
